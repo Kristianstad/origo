7ca93f0f7795056df288f8d9eac125ed83905ebc
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-03-16 13:17:20 +0100

    Update printControlForm.php

--- finalfs/origo/adm/functions/manage/printControlForm.php
+++ finalfs/origo/adm/functions/manage/printControlForm.php
@@ -1,4 +1,5 @@
 <?php
+
 	function printControlForm($control, $maps, $inheritPosts)
 	{
 		echo '<div><div style="float:left;"><form method="post" style="line-height:2">';
@@ -13,10 +14,11 @@
 		printInfoButton($control);
 		echo '</div></form></div>';
 		$deleteConfirmStr="Är du säker att du vill radera kontrollen ".$control['control']."? Referenser till kontrollen hanteras separat.";
-		printDeleteButton($control, $deleteConfirmStr, 'deleteButton1');
+		printDeleteButton($control, $deleteConfirmStr);
 		echo '</div><div style="clear:both">';
 		printAddOperation($control, array('maps'=>array_column($maps['maps'], 'map_id')), 'Lägg till i karta', $inheritPosts);
 		printRemoveOperation($control, $maps, 'Ta bort från karta', $inheritPosts);
 		echo '</div>';
 	}
+	
 ?>
