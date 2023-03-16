3a7eb207b8fc2d330ab7c6c6c539f7669bdff9a3
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-03-16 13:22:49 +0100

    Update printLayerForm.php

--- finalfs/origo/adm/functions/manage/printLayerForm.php
+++ finalfs/origo/adm/functions/manage/printLayerForm.php
@@ -1,5 +1,6 @@
 <?php
-	function printLayerForm($layer, $operationTables, $sources, $inheritPosts)
+
+	function printLayerForm($layer, $selectables, $operationTables, $sources, $inheritPosts)
 	{
 		echo '<div><div style="float:left;"><form method="post" style="line-height:2">';
 		printTextarea($layer, 'layer_id', 'textareaMedium', 'Id:');
@@ -37,27 +38,36 @@
 		}
 		printTextarea($layer, 'icon', 'textareaLarge', 'Ikon:');
 		printTextarea($layer, 'icon_extended', 'textareaLarge', 'Utfälld ikon:');
+		printUpdateSelect($layer, array('swiper'=>array("f", "t", "under")), 'miniSelect', 'Swiper-lager:');
 		echo '<br>';
 		if (isset($layer['layer']['type']) && $layer['layer']['type'] == 'WMS')
 		{
 			printTextarea($layer, 'format', 'textareaMedium', 'Format:');
 			printTextarea($layer, 'featureinfolayer', 'textareaMedium', 'FeatureInfo-lager:');
 		}
-		printTextarea($layer, 'style_filter', 'textareaLarge', 'Stilfilter:');
-		echo '<br>';
-		printTextarea($layer, 'style_config', 'textareaLarge', 'Stilkonfiguration:');
 		printTextarea($layer, 'attributes', 'textareaLarge', 'Attribut:');
 		echo '<br>';
-		printTextarea($layer, 'abstract', 'textareaLarge', 'Sammanfattning:');
-		printTextarea($layer, 'info', 'textareaLarge', 'Info:');
+		printTextarea($layer, 'style_config', 'textareaLarge', 'Stilkonfiguration:');
+		printTextarea($layer, 'style_filter', 'textareaLarge', 'Stilfilter:');
 		echo '<br>';
-		printTextarea($layer, 'categories', 'textareaLarge', 'Kategorier:');
-		printTextarea($layer, 'attribution', 'textareaLarge', 'Tillskrivning:');
-		echo '<br>';
+		printTextarea($layer, 'style_layer', 'textareaMedium', 'Stillager:');
 		printTextarea($layer, 'maxscale', 'textareaSmall', 'Maxskala:');
 		printTextarea($layer, 'minscale', 'textareaSmall', 'Minskala:');
-		printUpdateSelect($layer, array('swiper'=>array("f", "t", "under")), 'miniSelect', 'Swiper-lager:');
 		printTextarea($layer, 'exports', 'textareaMedium', 'Exportlager:');
+		echo '<br>';
+		printTextarea($layer, 'attribution', 'textareaLarge', 'Tillskrivning:');
+		echo '<hr style="border-style:dashed;border-width:1px 0 0 0;border-color:lightgray">';
+		printTextarea($layer, 'abstract', 'textareaLarge', 'Beskrivning:');
+		printTextarea($layer, 'resources', 'textareaMedium', 'Resurser:');
+		printUpdateSelect($layer, array('contact'=>$selectables['contacts']), 'bodySelect', 'Kontakt:');
+		echo '<br>';
+		printUpdateSelect($layer, array('origin'=>$selectables['origins']), 'bodySelect', 'Ursprungskälla:');
+		printTextarea($layer, 'updated', 'textareaMedium', 'Uppdaterad (åååå-mm-dd):');
+		printUpdateSelect($layer, array('update'=>$selectables['updates']), 'bodySelect', 'Uppdatering:');
+		printTextarea($layer, 'web', 'textareaMedium', 'Webbsida:');
+		echo '<hr style="border-style:dashed;border-width:1px 0 0 0;border-color:lightgray">';
+		printTextarea($layer, 'categories', 'textareaLarge', 'Kategorier:');
+		printTextarea($layer, 'info', 'textareaLarge', 'Info:');
 		if (isset($layer['layer']['type']))
 		{
 			if ($layer['layer']['type'] == 'GROUP')
@@ -76,18 +86,21 @@
 			}
 		}
 		printHiddenInputs($inheritPosts);
-		echo '<div class="buttonDiv">';
+		echo '<hr style="border-style:dashed;border-width:1px 0 0 0;border-color:lightgray">';
+		echo '<div class="buttonDiv" style="">';
 		printUpdateButton('layer');
 		$layer['layer']=$layer['layer']['layer_id'];
 		printInfoButton($layer);
 		echo '</div></form></div>';
 		$deleteConfirmStr="Är du säker att du vill radera lagret ".$layer['layer']."? Referenser till lagret hanteras separat.";
-		printDeleteButton($layer, $deleteConfirmStr, 'deleteButton6');
-		echo '</div><div style="clear:both">';
+		printDeleteButton($layer, $deleteConfirmStr);
+		echo '</div><div style="clear:both; margin-bottom:3rem">';
 		printAddOperation($layer, array('maps'=>array_column($operationTables['maps'], 'map_id')), 'Lägg till i karta', $inheritPosts);
 		printRemoveOperation($layer, array('maps'=>$operationTables['maps']), 'Ta bort från karta', $inheritPosts);
 		printAddOperation($layer, array('groups'=>array_column($operationTables['groups'], 'group_id')), 'Lägg till i grupp', $inheritPosts);
 		printRemoveOperation($layer, array('groups'=>$operationTables['groups']),'Ta bort från grupp', $inheritPosts);
 		echo '</div>';
 	}
+	
 ?>
+
