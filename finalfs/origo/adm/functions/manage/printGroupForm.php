10ec007f44905e5107d4371990f81cef10a623be
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-04-06 13:32:29 +0200

    Update printGroupForm.php

--- finalfs/origo/adm/functions/manage/printGroupForm.php
+++ finalfs/origo/adm/functions/manage/printGroupForm.php
@@ -1,5 +1,14 @@
 <?php
 
+	require_once("./functions/manage/printTextarea.php");
+	require_once("./functions/manage/printUpdateSelect.php");
+	require_once("./functions/manage/printHiddenInputs.php");
+	require_once("./functions/manage/printUpdateButton.php");
+	require_once("./functions/manage/printInfoButton.php");
+	require_once("./functions/manage/printDeleteButton.php");
+	require_once("./functions/manage/printAddOperation.php");
+	require_once("./functions/manage/printRemoveOperation.php");
+
 	function printGroupForm($group, $operationTables, $inheritPosts)
 	{
 		echo '<div><div style="float:left;"><form method="post" style="line-height:2">';
@@ -18,7 +27,7 @@
 		printInfoButton($group);
 		echo '</div></form></div>';
 		$deleteConfirmStr="Är du säker på att du vill radera gruppen ".$group['group']."? Ingående lager påverkas ej och referenser till gruppen hanteras separat.";
-		printDeleteButton($group, $deleteConfirmStr);
+		printDeleteButton($group, $deleteConfirmStr, $inheritPosts);
 		echo '</div><div style="clear:both">';
 		printAddOperation($group, array('maps'=>array_column($operationTables['maps'], 'map_id')), 'Lägg till i karta', $inheritPosts);
 		printRemoveOperation($group, array('maps'=>$operationTables['maps']), 'Ta bort från karta', $inheritPosts);
