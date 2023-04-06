379cda8523062d0f6c0e88489f0b9ee81f7997cf
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-04-06 12:55:24 +0200

    Update and rename CONNECTION_STRING.php to dbhConnectionString.php

--- finalfs/origo/adm/constants/CONNECTION_STRING.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-	define('CONNECTION_STRING', 'host=localhost port=5432 dbname=origo user=postgres password=postgres');
-
-?>
---
+++ finalfs/origo/adm/constants/dbhConnectionString.php
@@ -0,0 +1,5 @@
+<?php
+
+	$dbhConnectionString = 'host=localhost port=5432 dbname=origo user=postgres password=postgres' ;
+
+?>
