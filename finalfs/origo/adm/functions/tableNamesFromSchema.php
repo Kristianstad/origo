7e70541234bc0d60b41a2ed325bf65a660a56482
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-04-06 13:14:43 +0200

    Update tableNamesFromSchema.php

--- finalfs/origo/adm/functions/tableNamesFromSchema.php
+++ finalfs/origo/adm/functions/tableNamesFromSchema.php
@@ -1,4 +1,5 @@
 <?php
+
 	function tableNamesFromSchema($dbh, $schema)
 	{
 		$result=pg_query($dbh, "SELECT table_name FROM information_schema.tables WHERE table_schema='$schema' AND table_type='BASE TABLE'");
@@ -8,4 +9,5 @@
 		}
 		return pg_fetch_all_columns($result);
 	}
+	
 ?>
