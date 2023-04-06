d08c35eb88e783df9ca7be2ead8bd4a0be347b3d
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-04-06 13:37:40 +0200

    Update printChildSelect.php

--- finalfs/origo/adm/functions/manage/printChildSelect.php
+++ finalfs/origo/adm/functions/manage/printChildSelect.php
@@ -1,4 +1,9 @@
 <?php
+
+	require_once("./functions/pkColumnOfTable.php");
+	require_once("./functions/manage/printSelectOptions.php");
+	require_once("./functions/manage/printHiddenInputs.php");
+
 	function printChildSelect($target, $column, &$thClass, $heading, $inheritPosts, $groupLevel=1, $selectedValue=null)
 	{
 		$targetColumnValue=trim(current($target)[$column], '{}');
@@ -7,7 +12,7 @@
 			$groupIdsArray=array();
 			$columnType=rtrim($column, 's');
 			$sName=$columnType.'Id';
-			$columnArr=pgArrayToPhp('{'.$targetColumnValue.'}');
+			$columnArr=explode(',', $targetColumnValue);
 			if (key($target) == 'group' && ($column == 'groups' || $column == 'layers'))
 			{
 				if (isset($inheritPosts['groupIds']))
@@ -48,29 +53,49 @@
 			$targetId=current($target)[pkColumnOfTable(key($target).'s')];
 			$ucColumn=ucfirst($column);
 			$hiddenInputs=array();
-			if (isset($inheritPosts['mapId']))
+			if ($column == 'schemas')
 			{
-				$hiddenInputs['mapId']=$inheritPosts['mapId'];
+				$hiddenInputs['databaseId']=$inheritPosts['databaseId'];
+				$optionLabels = array_merge(array(""), preg_filter('/^'.current($target)['database_id'].'[.]/', '', $options));
+				$options=array_combine($options, $optionLabels);
 			}
-			if ($column == 'layers')
+			elseif ($column == 'tables')
 			{
-				if (isset($inheritPosts['groupIds']))
+				if (isset($inheritPosts['databaseId']))
 				{
-					$hiddenInputs['groupIds']=implode(',', $groupIdsArray);
+					$hiddenInputs['databaseId']=$inheritPosts['databaseId'];
+				}
+				$hiddenInputs['schemaId']=$inheritPosts['schemaId'];
+				$optionLabels = array_merge(array(""), preg_filter('/^'.current($target)['schema_id'].'[.]/', '', $options));
+				$options=array_combine($options, $optionLabels);
+			}
+			else
+			{
+				if (isset($inheritPosts['mapId']))
+				{
+					$hiddenInputs['mapId']=$inheritPosts['mapId'];
+				}
+				if ($column == 'layers')
+				{
+					if (isset($inheritPosts['groupIds']))
+					{
+						$hiddenInputs['groupIds']=implode(',', $groupIdsArray);
+					}
 				}
 			}
+			$formId=$column.$groupLevel.'HeadForm';
 			echo <<<HERE
 				<th class="{$thClass}">
 					<h3 class="{$edith3Class}">{$heading}</h3>
 					<div style="display:flex">
-						<form class="headForm" method="post">
+						<form id="{$formId}" class="headForm" method="post">
 							<select onchange='this.form.submit();' class='headSelect' id='{$targetId}{$ucColumn}' name='{$sName}'>
 			HERE;
 			printSelectOptions($options, $selectedValue);
 			echo           "</select>&nbsp;";
 			printHiddenInputs($hiddenInputs);
 			echo <<<HERE
-							<button type="submit" class="headButton" name="{$columnType}Button" value="get">
+							<button type="submit" class="headButton" name="{$columnType}Button" value="get" style="margin-left:-0.5em">
 								Hämta
 							</button>
 						</form>
@@ -80,4 +105,5 @@
 			$thClass='thNext';
 		}
 	}
+	
 ?>
