2da9ddcd934ad43a8c4615f6c6eff1d2dc7929ef
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-04-06 13:10:24 +0200

    Update toSwedish.php

--- finalfs/origo/adm/functions/toSwedish.php
+++ finalfs/origo/adm/functions/toSwedish.php
@@ -2,33 +2,10 @@
 
 	function toSwedish($engStr)
 	{
-		$translateArray=array(
-			'map'		=>'karta',
-			'maps'		=>'kartor',
-			'control'	=>'kontroll',
-			'controls'	=>'kontroller',
-			'group'		=>'grupp',
-			'groups'	=>'grupper',
-			'layer'		=>'lager',
-			'layers'	=>'lager',
-			'footer'	=>'sidfot',
-			'footers'	=>'sidfötter',
-			'source'	=>'källa',
-			'sources'	=>'källor',
-			'service'	=>'tjänst',
-			'services'	=>'tjänster',
-			'contact'	=>'kontakt',
-			'contacts'	=>'kontakter',
-			'export'	=>'exportlager',
-			'exports'	=>'exportlager',
-			'update'	=>'uppdateringsrutin',
-			'updates'	=>'uppdateringar',
-			'origin'	=>'ursprungskälla',
-			'origins'	=>'ursprungskällor'
-		);
-		if (isset($translateArray[$engStr]))
+		require("./constants/swedishDic.php");
+		if (isset($swedishDic[$engStr]))
 		{
-			return $translateArray[$engStr];
+			return $swedishDic[$engStr];
 		}
 		else
 		{
