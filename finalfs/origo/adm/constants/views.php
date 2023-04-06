fae45e94fa4225c6e1273714bfc3d2bb521e2c49
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-04-06 12:56:53 +0200

    Create views.php

---
+++ finalfs/origo/adm/constants/views.php
@@ -0,0 +1,9 @@
+<?php
+
+	$views = array(
+		'Allt'	=> array(),
+		'Origo' => array('maps', 'controls', 'groups', 'layers', 'sources', 'services', 'tilegrids', 'footers', 'proj4defs'),
+		'Meta'	=> array('databases', 'schemas', 'tables', 'contacts', 'origins', 'updates')
+	);
+
+?>
