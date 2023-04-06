0e991230fca68a08ef78831bbd27e9082cb9ecd5
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-04-06 12:58:01 +0200

    Create swedishDic.php

---
+++ finalfs/origo/adm/constants/swedishDic.php
@@ -0,0 +1,33 @@
+<?php
+
+	$swedishDic=array(
+		'map'		=>'karta',
+		'maps'		=>'kartor',
+		'control'	=>'kontroll',
+		'controls'	=>'kontroller',
+		'group'		=>'grupp',
+		'groups'	=>'grupper',
+		'layer'		=>'lager',
+		'layers'	=>'lager',
+		'footer'	=>'sidfot',
+		'footers'	=>'sidfötter',
+		'source'	=>'källa',
+		'sources'	=>'källor',
+		'service'	=>'tjänst',
+		'services'	=>'tjänster',
+		'contact'	=>'kontakt',
+		'contacts'	=>'kontakter',
+		'export'	=>'exportlager',
+		'exports'	=>'exportlager',
+		'update'	=>'uppdateringsrutin',
+		'updates'	=>'uppdateringar',
+		'origin'	=>'ursprungskälla',
+		'origins'	=>'ursprungskällor',
+		'schemas'	=>'scheman',
+		'table'		=>'tabell',
+		'tables'	=>'tabeller',
+		'database'	=>'databas',
+		'databases'	=>'databaser'
+	);
+	
+?>
