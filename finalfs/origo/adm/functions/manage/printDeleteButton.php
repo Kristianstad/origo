3150a83861906112faf82a0b95e00529d5004b2e
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-04-06 13:30:15 +0200

    Update printDeleteButton.php

--- finalfs/origo/adm/functions/manage/printDeleteButton.php
+++ finalfs/origo/adm/functions/manage/printDeleteButton.php
@@ -1,17 +1,45 @@
 <?php
 
-	function printDeleteButton($target, $deleteConfirmStr)
+	function printDeleteButton($target, $deleteConfirmStr, $inheritPosts)
 	{
 		$targetType=key($target);
 		$targetId=current($target);
+		if ($targetType == 'map')
+		{
+			$inheritPosts=array();
+		}
+		elseif ($targetType == 'group')
+		{
+			$groupIdsArr=explode(',', $inheritPosts['groupIds']);
+			foreach (array_reverse($groupIdsArr, true) as $k => $v)
+			{
+				unset($groupIdsArr[$k]);
+				if ($v == $targetId)
+				{
+					break;
+				}
+			}
+			$inheritPosts['groupIds']=implode(',', $groupIdsArr);
+		}
+		else
+		{
+			foreach ($inheritPosts as $k => $v)
+			{
+				unset($inheritPosts[$k]);
+				if ($k == $targetType.'Id' && $v == $targetId)
+				{
+					break;
+				}
+			}
+		}
 		echo <<<HERE
 			<div class="deleteButtonDiv">
 				<form method='post' onsubmit='confirmStr="{$deleteConfirmStr}"; return confirm(confirmStr);' style='line-height:2'>
 					<input type="hidden" name="{$targetType}IdDel" value="{$targetId}">
 					<button class='deleteButton' type='submit' name='{$targetType}Button' value='delete'><img class="resizeimg" src onerror="this.parentNode.style.marginTop=this.parentNode.parentNode.parentNode.previousElementSibling.offsetHeight+'px'; this.parentNode.style.visibility='visible';">Radera</button>
-				</form>
-			</div>
 		HERE;
+		printHiddenInputs($inheritPosts);
+		echo '</form></div>';
 	}
 	
 ?>
