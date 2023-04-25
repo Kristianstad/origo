4c8e307ccfbbee0380c74545cc8a56d44c0e1f36
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-04-25 10:18:38 +0200

    Create updatedTarget.php

---
+++ finalfs/origo/adm/functions/manage/updatedTarget.php
@@ -0,0 +1,22 @@
+<?php
+
+	function updatedTarget($target, $updatePosts)
+	{
+		require("./constants/arrayColumns.php");
+		$columns=current($target);
+		foreach ($columns as $column=>$value)
+		{
+			$newValue=$updatePosts['update'.ucfirst($column)];
+			if (isset($newValue))
+			{
+				if (in_array($column, $arrayColumns))
+				{
+					$newValue='{'.$newValue.'}';
+				}
+				$columns[$column]=$newValue;
+			}
+		}
+		return array(key($target)=>$columns);
+	}
+	
+?>
