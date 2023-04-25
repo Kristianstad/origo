6a7a815cbd945c88fede1ce98c4ea2cc37db0c49
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-04-25 10:08:42 +0200

    Update printReadDbSchemasButton.php

--- finalfs/origo/adm/functions/manage/printReadDbSchemasButton.php
+++ finalfs/origo/adm/functions/manage/printReadDbSchemasButton.php
@@ -4,7 +4,7 @@
 	{
 		$confirmStr="Läser in eventuella nya scheman från databasen (utan tabeller). Befintiga metadata påverkas ej.";
 		echo <<<HERE
-			<form onsubmit='confirmStr="{$confirmStr}"; confirm(confirmStr); setTimeout(function() {document.getElementById("databasesHeadForm").submit();}, 1000);' action="read_db_schemas.php" method="get" target="hiddenFrame">
+			<form id='readDbSchemas' onsubmit='confirmStr="{$confirmStr}"; confirm(confirmStr); setTimeout(function() {document.getElementById("databasesHeadForm").submit();}, 1000);' action="read_db_schemas.php" method="get" target="hiddenFrame">
 				<button class="updateButton" type="submit" name="database" value="{$databaseId}">
 					Läs in nya scheman
 				</button>
