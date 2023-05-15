525911eaa31f3b3a40a35eab13f7e5d6dab6c55b
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-05-15 13:37:51 +0200

    Update printRemoveOperation.php

--- finalfs/origo/adm/functions/manage/printRemoveOperation.php
+++ finalfs/origo/adm/functions/manage/printRemoveOperation.php
@@ -10,9 +10,9 @@
 		if (!empty($parents))
 		{
 			echo '<form class="addForm" method="post">';
-			echo '<select class="headSelect" name="from'.ucfirst(rtrim(key($tableToRemoveFrom), 's')).'Id">';
+			echo '<select class="addSelect" name="from'.ucfirst(rtrim(key($tableToRemoveFrom), 's')).'Id">';
 			printSelectOptions(array_merge(array(""), $parents));
-			echo '</select>';
+			echo '</select>&nbsp;';
 			printHiddenInputs($inheritPosts);
 			echo '<button type="submit" name="'.key($targetToRemove).'Button" value="operation">'.$buttontext.'</button>';
 			echo '</form>';
