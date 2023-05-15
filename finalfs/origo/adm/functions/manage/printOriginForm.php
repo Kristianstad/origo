4965f66befcad1aa1a442fa7b98e474f068d6c7a
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-05-15 13:29:10 +0200

    Update printOriginForm.php

--- finalfs/origo/adm/functions/manage/printOriginForm.php
+++ finalfs/origo/adm/functions/manage/printOriginForm.php
@@ -8,7 +8,7 @@
 
 	function printOriginForm($origin, $inheritPosts)
 	{
-		echo '<div><div style="float:left;"><form method="post" style="line-height:2">';
+		echo '<div><div class="printXFormDiv"><form method="post">';
 		printTextarea($origin, 'origin_id', 'textareaMedium', 'Id:');
 		printTextarea($origin, 'name', 'textareaMedium', 'Namn:');
 		printTextarea($origin, 'web', 'textareaMedium', 'Webbsida:');
