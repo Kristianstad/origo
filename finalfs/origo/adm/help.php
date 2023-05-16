b02c92b2ebafc5d3cbc94180989bfe6c92d3f661
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-05-16 16:05:56 +0200

    Update help.php

--- finalfs/origo/adm/help.php
+++ finalfs/origo/adm/help.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<style>
+		<?php require("./styles/help.css"); ?>
+	</style>
+</head>
 <body>
 	<a href="https://origo-map.github.io/origo-documentation/latest/#origo-map" target="_blank">Origo-dokumentation</a>
 </body>
