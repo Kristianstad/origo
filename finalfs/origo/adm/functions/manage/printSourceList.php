7124dae2bfe512e0bf7a0005a3f69bcdf1cfcafb
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-05-23 12:26:43 +0200

    Update printSourceList.php

--- finalfs/origo/adm/functions/manage/printSourceList.php
+++ finalfs/origo/adm/functions/manage/printSourceList.php
@@ -7,12 +7,13 @@
 		$layerId=current($layer)['layer_id'];
 		$layerSource=current($layer)['source'];
 		echo <<<HERE
-			<label for="{$layerId}Source">Källa:</label>
-			<input type="text" list="sourcelist" class="bodySelect" id="{$layerId}Source" name="updateSource" value="{$layerSource}" onfocus="this.value='';" />
-			<datalist id="sourcelist">
+			<span>
+				<label for="{$layerId}Source">Källa:</label>
+				<input type="text" list="sourcelist" class="bodySelect" id="{$layerId}Source" name="updateSource" value="{$layerSource}" onfocus="this.value='';" />
+				<datalist id="sourcelist">
 		HERE;
 		printSelectOptions(array_merge(array(""), $sources), $layerSource);
-		echo '</datalist>&nbsp;';
+		echo '</datalist></span><wbr>';
 	}
 	
 ?>
