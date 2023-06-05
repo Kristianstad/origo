ef162018059bde76a14cda8460a5e75304ee3610
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-06-05 12:33:35 +0200

    Update updatedTarget.php

--- finalfs/origo/adm/functions/manage/updatedTarget.php
+++ finalfs/origo/adm/functions/manage/updatedTarget.php
@@ -7,16 +7,17 @@
 		foreach ($columns as $column=>$value)
 		{
 			$newValue=$updatePosts['update'.ucfirst($column)];
-			if (isset($newValue))
+			if (!isset($newValue))
 			{
-				if (in_array($column, $arrayColumns))
-				{
-					$newValue='{'.$newValue.'}';
-				}
-				$columns[$column]=$newValue;
+				$newValue='';
 			}
+			if (in_array($column, $arrayColumns))
+			{
+				$newValue='{'.$newValue.'}';
+			}
+			$columns[$column]=$newValue;
 		}
 		return array(key($target)=>$columns);
 	}
-	
+
 ?>
