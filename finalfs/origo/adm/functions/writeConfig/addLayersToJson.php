a7010923a87e159676142b6a26181c1dbe9ce8e2
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-06-12 13:32:33 +0200

    bugfix

--- finalfs/origo/adm/functions/writeConfig/addLayersToJson.php
+++ finalfs/origo/adm/functions/writeConfig/addLayersToJson.php
@@ -209,7 +209,7 @@
 				{
 					$json = $json.', "abstract": "';
 				}
-				$adminForm="<form action='".$_SERVER["HTTP_REFERER"]."?view=Origo' method='post' target='_blank'><button type='submit' name='layerId' value='".$layer['layer_id']."' style='float:right; color:blue'>Administrera</button></form>";
+				$adminForm="<form action='".$_SERVER["HTTP_REFERER"]."' method='post' target='_blank'><button type='submit' name='layerId' value='".$layer['layer_id']."' style='float:right; color:blue'>Administrera</button></form>";
 				$json = $json.'<br>'.$adminForm.'"';
 				if (!empty($layer['attributes']) && $layer['type'] !== 'GROUP')
 				{
