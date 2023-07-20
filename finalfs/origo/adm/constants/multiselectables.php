0149417e4d4128e6edaff7bdd4ba8557268493d4
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-07-20 14:23:09 +0200

    Update multiselectables.php

--- finalfs/origo/adm/constants/multiselectables.php
+++ finalfs/origo/adm/constants/multiselectables.php
@@ -1,5 +1,5 @@
 <?php
 
-	$multiselectables=array('controls', 'groups', 'layers', 'proj4defs', 'exports');
-	
+	$multiselectables=array('controls', 'groups', 'layers', 'proj4defs', 'exports', 'keywords');
+
 ?>
