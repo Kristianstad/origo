014adf489fde413b29a77bc89d6d8cf17d4cfef8
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-08-29 13:53:39 +0200

    Update printTextarea.php

--- finalfs/origo/adm/functions/manage/printTextarea.php
+++ finalfs/origo/adm/functions/manage/printTextarea.php
@@ -1,9 +1,11 @@
 <?php
 
 	require_once("./functions/pkColumnOfTable.php");
+	require_once("./functions/manage/printMultiselectButton.php");
 
 	function printTextarea($target, $column, $class, $label, $readonly='no')
 	{
+		require("./constants/multiselectables.php");
 		$targetId=current($target)[pkColumnOfTable(key($target).'s')];
 		$columnValue=current($target)[$column];
 		if (preg_match('/^\{([^"\{\[]*("[^:])?)*\}$/', $columnValue))
@@ -23,8 +25,12 @@
 			<span>
 				<label for="{$targetId}{$ucColumn}">{$label}</label>
 				<textarea {$readonly}rows="1" class="{$class}" id="{$targetId}{$ucColumn}" name="update{$ucColumn}">{$columnValue}</textarea>
-			</span><wbr>
 		HERE;
+		if (in_array($column, $multiselectables))
+		{
+			printMultiselectButton($column, trim(current($target)[$column], '{}'), '+', 'margin-left:-0.5em');
+		}
+		echo '</span><wbr>';
 	}
-	
+
 ?>
