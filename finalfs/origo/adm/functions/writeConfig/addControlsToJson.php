43924e73986a329b62b8a9c2aceeb71a3483451e
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-09-06 14:00:45 +0200

    Update addControlsToJson.php

--- finalfs/origo/adm/functions/writeConfig/addControlsToJson.php
+++ finalfs/origo/adm/functions/writeConfig/addControlsToJson.php
@@ -1,31 +1,34 @@
 <?php
 
-function addControlsToJson()
-{
-	GLOBAL $json, $map, $controls;
-	$mapControls = pgArrayToPhp($map['controls']);
-	$json = $json.'"controls": [';
-	$firstControl = true;
-	foreach ($mapControls as $control)
+	require_once("./functions/pgArrayToPhp.php");
+	require_once("./functions/array_column_search.php");
+
+	function addControlsToJson()
 	{
-		if ($firstControl)
+		GLOBAL $json, $map, $controls;
+		$mapControls = pgArrayToPhp($map['controls']);
+		$json = $json.'"controls": [';
+		$firstControl = true;
+		foreach ($mapControls as $control)
 		{
-			$firstControl = false;
+			if ($firstControl)
+			{
+				$firstControl = false;
+			}
+			else
+			{
+				$json = $json.', ';
+			}
+			$control = array_column_search($control, 'control_id', $controls);
+			$controlName = trim(explode('#', $control['control_id'], 2)[0]);
+			$json = $json.'{ "name": "'.$controlName.'"';
+			if (!empty($control['options']) && $control['options'] !== 'null')
+			{
+				$json = $json.', "options": '.$control['options'];
+			}
+			$json = $json.' }';
 		}
-		else
-		{
-			$json = $json.', ';
-		}
-		$control = array_column_search($control, 'control_id', $controls);
-		$controlName = trim(explode('#', $control['control_id'], 2)[0]);
-		$json = $json.'{ "name": "'.$controlName.'"';
-		if (!empty($control['options']) && $control['options'] !== 'null')
-		{
-			$json = $json.', "options": '.$control['options'];
-		}
-		$json = $json.' }';
+		$json = $json.']';
 	}
-	$json = $json.']';
-}
 
 ?>
