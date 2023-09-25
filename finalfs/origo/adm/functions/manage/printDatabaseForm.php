3ac573eb22c4925fda35027e6455810ec4838fad
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-09-25 11:56:19 +0200

    Update printDatabaseForm.php

--- finalfs/origo/adm/functions/manage/printDatabaseForm.php
+++ finalfs/origo/adm/functions/manage/printDatabaseForm.php
@@ -1,5 +1,6 @@
 <?php
 
+	require_once("./functions/manage/isFullTarget.php");
 	require_once("./functions/manage/printTextarea.php");
 	require_once("./functions/manage/printHiddenInputs.php");
 	require_once("./functions/manage/printUpdateButton.php");
@@ -7,23 +8,26 @@
 	require_once("./functions/manage/printReadDbSchemasButton.php");
 	require_once("./functions/manage/printDeleteButton.php");
 
-	function printDatabaseForm($database, $inheritPosts)
+	function printDatabaseForm($database, $inheritPosts, $helps=array())
 	{
+		if (!isFullTarget($database))
+		{
+			die("printDatabaseForm($database, $inheritPosts, $helps=array()) failed!");
+		}
 		echo '<div><div class="printXFormDiv"><form method="post">';
-		printTextarea($database, 'database_id', 'textareaMedium', 'Id:');
-		printTextarea($database, 'connectionstring', 'textareaLarge', 'Anslutningssträng:');
-		printTextarea($database, 'abstract', 'textareaLarge', 'Beskrivning:');
-		printTextarea($database, 'info', 'textareaLarge', 'Info:');
+		printTextarea($database, 'database_id', 'textareaMedium', 'Id:', in_array('database_id', $helps));
+		printTextarea($database, 'connectionstring', 'textareaLarge', 'Anslutningssträng:', in_array('connectionstring', $helps));
+		printTextarea($database, 'abstract', 'textareaLarge', 'Beskrivning:', in_array('abstract', $helps));
+		printTextarea($database, 'info', 'textareaLarge', 'Info:', in_array('info', $helps));
 		printHiddenInputs($inheritPosts);
 		echo '<div class="buttonDiv">';
 		printUpdateButton('database');
 		$database['database']=$database['database']['database_id'];
 		printInfoButton($database);
 		printReadDbSchemasButton($database['database']);
-		echo '</div></form></div>';
 		$deleteConfirmStr="Är du säker att du vill radera all metadata för databasen ".$database['database']."? Metadata för ingående scheman och tabeller hanteras separat.";
 		printDeleteButton($database, $deleteConfirmStr, $inheritPosts);
-		echo '</div>';
+		echo '</div></form></div></div>';
 	}
-	
+
 ?>
