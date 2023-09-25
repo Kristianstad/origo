756824ff2c21c2d686eddaef3ac088932ec0742a
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-09-25 11:45:38 +0200

    Update views.php

--- finalfs/origo/adm/constants/views.php
+++ finalfs/origo/adm/constants/views.php
@@ -3,7 +3,8 @@
 	$views = array(
 		'Allt'	=> array(),
 		'Origo' => array('maps', 'controls', 'groups', 'layers', 'sources', 'services', 'tilegrids', 'footers', 'proj4defs'),
-		'Meta'	=> array('databases', 'schemas', 'tables', 'contacts', 'origins', 'updates', 'keywords')
+		'Meta'	=> array('databases', 'schemas', 'tables', 'contacts', 'origins', 'updates', 'keywords'),
+		'Verktyg' => array('helps')
 	);
 
 ?>
