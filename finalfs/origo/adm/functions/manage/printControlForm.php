bf3f373c987422c27895e197515bb2862b1d248f
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-09-25 12:00:59 +0200

    Update printControlForm.php

--- finalfs/origo/adm/functions/manage/printControlForm.php
+++ finalfs/origo/adm/functions/manage/printControlForm.php
@@ -1,5 +1,6 @@
 <?php
 
+	require_once("./functions/manage/isFullTarget.php");
 	require_once("./functions/manage/printTextarea.php");
 	require_once("./functions/manage/printHiddenInputs.php");
 	require_once("./functions/manage/printUpdateButton.php");
@@ -8,26 +9,29 @@
 	require_once("./functions/manage/printAddOperation.php");
 	require_once("./functions/manage/printRemoveOperation.php");
 
-	function printControlForm($control, $maps, $inheritPosts)
+	function printControlForm($control, $maps, $inheritPosts, $helps=array())
 	{
+		if (!isFullTarget($control))
+		{
+			die("printControlForm($control, $maps, $inheritPosts, $helps=array()) failed!");
+		}
 		echo '<div><div class="printXFormDiv"><form method="post">';
-		printTextarea($control, 'control_id', 'textareaMedium', 'Id:');
-		printTextarea($control, 'abstract', 'textareaLarge', 'Beskrivning:');
-		printTextarea($control, 'options', 'textareaLarge', 'Inställningar:');
-		printTextarea($control, 'info', 'textareaLarge', 'Info:');
+		printTextarea($control, 'control_id', 'textareaMedium', 'Id:', in_array('control_id', $helps));
+		printTextarea($control, 'abstract', 'textareaLarge', 'Beskrivning:', in_array('abstract', $helps));
+		printTextarea($control, 'options', 'textareaLarge', 'Inställningar:', in_array('options', $helps));
+		printTextarea($control, 'info', 'textareaLarge', 'Info:', in_array('info', $helps));
 		printHiddenInputs($inheritPosts);
 		echo '<hr class="dashedHr">';
 		echo '<div class="buttonDiv">';
 		printUpdateButton('control');
 		$control['control']=$control['control']['control_id'];
 		printInfoButton($control);
-		echo '</div></form></div>';
 		$deleteConfirmStr="Är du säker att du vill radera kontrollen ".$control['control']."? Referenser till kontrollen hanteras separat.";
 		printDeleteButton($control, $deleteConfirmStr, $inheritPosts);
-		echo '</div><div class="addRemoveDiv">';
+		echo '</div></form></div></div><div class="addRemoveDiv">';
 		printAddOperation($control, array('maps'=>array_column($maps['maps'], 'map_id')), 'Lägg till i karta', $inheritPosts);
 		printRemoveOperation($control, $maps, 'Ta bort från karta', $inheritPosts);
 		echo '</div>';
 	}
-	
+
 ?>
