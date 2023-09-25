ecdd51c3446d1ecb224a7a283b2af7eebc9e47b5
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-09-25 11:58:38 +0200

    Update printSourceList.php

--- finalfs/origo/adm/functions/manage/printSourceList.php
+++ finalfs/origo/adm/functions/manage/printSourceList.php
@@ -1,19 +1,32 @@
 <?php
 
+	require_once("./functions/manage/isFullTarget.php");
+	require_once("./functions/manage/targetId.php");
+	require_once("./functions/manage/targetConfigParam.php");
 	require_once("./functions/manage/printSelectOptions.php");
+	require_once("./functions/manage/printHelpButton.php");
 
-	function printSourceList($layer, $sources)
+	function printSourceList($layer, $sources, $help=false)
 	{
-		$layerId=current($layer)['layer_id'];
-		$layerSource=current($layer)['source'];
+		if (!isFullTarget($layer))
+		{
+			die("printSourceList($layer, $sources) failed!");
+		}
+		$layerId=targetId($layer);
+		$layerSource=targetConfigParam($layer, 'source');
 		echo <<<HERE
 			<span>
-				<label for="{$layerId}Source">Källa:</label>
+				<label title="layer:source" for="{$layerId}Source">Källa:</label>
 				<input type="text" list="sourcelist" class="bodySelect" id="{$layerId}Source" name="updateSource" value="{$layerSource}" onfocus="this.value='';" />
 				<datalist id="sourcelist">
 		HERE;
 		printSelectOptions(array_merge(array(""), $sources), $layerSource);
-		echo '</datalist></span><wbr>';
+		echo '</datalist>';
+		if ($help)
+		{
+			printHelpButton('layer', 'source');
+		}
+		echo '</span><wbr>';
 	}
-	
+
 ?>
