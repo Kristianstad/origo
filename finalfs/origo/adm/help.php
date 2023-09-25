de08b34a55d3e3a68ccb8e85ecf5ffa169cde8b5
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-09-25 11:41:38 +0200

    Update help.php

--- finalfs/origo/adm/help.php
+++ finalfs/origo/adm/help.php
@@ -6,6 +6,27 @@
 	</style>
 </head>
 <body>
-	<a href="https://origo-map.github.io/origo-documentation/latest/#origo-map" target="_blank">Origo-dokumentation</a>
+	<?php
+		if (isset($_GET['id']))
+		{
+			require_once("./functions/dbh.php");
+			require_once("./functions/array_column_search.php");
+			require_once("./functions/all_from_table.php");
+			require("./constants/configSchema.php");
+			$dbh=dbh();
+			$helps=all_from_table($dbh, $configSchema, 'helps');
+			$help=array_column_search($_GET['id'], 'help_id', $helps);
+			if (isset($help['abstract']))
+			{
+				echo $help['abstract'];
+			}
+		}
+		else
+		{
+			echo <<<HERE
+				<a href="https://origo-map.github.io/origo-documentation/latest/#origo-map" target="_blank">Origo-dokumentation</a>
+			HERE;
+		}
+	?>
 </body>
 </html>
