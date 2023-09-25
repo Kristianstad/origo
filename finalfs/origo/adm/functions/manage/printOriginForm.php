de1d62170dd142a83d4379989cb540af104a356d
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-09-25 11:51:05 +0200

    Update printOriginForm.php

--- finalfs/origo/adm/functions/manage/printOriginForm.php
+++ finalfs/origo/adm/functions/manage/printOriginForm.php
@@ -1,29 +1,33 @@
 <?php
 
+	require_once("./functions/manage/isFullTarget.php");
 	require_once("./functions/manage/printTextarea.php");
 	require_once("./functions/manage/printHiddenInputs.php");
 	require_once("./functions/manage/printUpdateButton.php");
 	require_once("./functions/manage/printInfoButton.php");
 	require_once("./functions/manage/printDeleteButton.php");
 
-	function printOriginForm($origin, $inheritPosts)
+	function printOriginForm($origin, $inheritPosts, $helps=array())
 	{
+		if (!isFullTarget($origin))
+		{
+			die("printOriginForm($origin, $inheritPosts, $helps=array()) failed!");
+		}
 		echo '<div><div class="printXFormDiv"><form method="post">';
-		printTextarea($origin, 'origin_id', 'textareaMedium', 'Id:');
-		printTextarea($origin, 'name', 'textareaMedium', 'Namn:');
-		printTextarea($origin, 'web', 'textareaMedium', 'Webbsida:');
-		printTextarea($origin, 'email', 'textareaMedium', 'E-mail:');
-		printTextarea($origin, 'abstract', 'textareaLarge', 'Beskrivning:');
-		printTextarea($origin, 'info', 'textareaLarge', 'Info:');
+		printTextarea($origin, 'origin_id', 'textareaMedium', 'Id:', in_array('origin_id', $helps));
+		printTextarea($origin, 'name', 'textareaMedium', 'Namn:', in_array('name', $helps));
+		printTextarea($origin, 'web', 'textareaMedium', 'Webbsida:', in_array('web', $helps));
+		printTextarea($origin, 'email', 'textareaMedium', 'E-mail:', in_array('email', $helps));
+		printTextarea($origin, 'abstract', 'textareaLarge', 'Beskrivning:', in_array('abstract', $helps));
+		printTextarea($origin, 'info', 'textareaLarge', 'Info:', in_array('info', $helps));
 		printHiddenInputs($inheritPosts);
 		echo '<div class="buttonDiv">';
 		printUpdateButton('origin');
 		$origin['origin']=$origin['origin']['origin_id'];
 		printInfoButton($origin);
-		echo '</div></form></div>';
 		$deleteConfirmStr="Är du säker att du vill radera ursprungskällan ".$origin['origin']."? Referenser till ursprungskällan hanteras separat.";
 		printDeleteButton($origin, $deleteConfirmStr, $inheritPosts);
-		echo '</div>';
+		echo '</div></form></div></div>';
 	}
-	
+
 ?>
