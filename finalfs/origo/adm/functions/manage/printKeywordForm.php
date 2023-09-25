7509a3b2396eecfce2b633a1fd5702bb275fa857
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-09-25 12:07:22 +0200

    Update printKeywordForm.php

--- finalfs/origo/adm/functions/manage/printKeywordForm.php
+++ finalfs/origo/adm/functions/manage/printKeywordForm.php
@@ -1,26 +1,30 @@
 <?php
 
+	require_once("./functions/manage/isFullTarget.php");
 	require_once("./functions/manage/printTextarea.php");
 	require_once("./functions/manage/printHiddenInputs.php");
 	require_once("./functions/manage/printUpdateButton.php");
 	require_once("./functions/manage/printInfoButton.php");
 	require_once("./functions/manage/printDeleteButton.php");
 
-	function printKeywordForm($keyword, $inheritPosts)
+	function printKeywordForm($keyword, $inheritPosts, $helps=array())
 	{
+		if (!isFullTarget($keyword))
+		{
+			die("printKeywordForm($keyword, $inheritPosts, $helps=array()) failed!");
+		}
 		echo '<div><div class="printXFormDiv"><form method="post">';
-		printTextarea($keyword, 'keyword_id', 'textareaMedium', 'Id:');
-		printTextarea($keyword, 'abstract', 'textareaLarge', 'Beskrivning:');
-		printTextarea($keyword, 'info', 'textareaLarge', 'Info:');
+		printTextarea($keyword, 'keyword_id', 'textareaMedium', 'Id:', in_array('keyword_id', $helps));
+		printTextarea($keyword, 'abstract', 'textareaLarge', 'Beskrivning:', in_array('abstract', $helps));
+		printTextarea($keyword, 'info', 'textareaLarge', 'Info:', in_array('info', $helps));
 		printHiddenInputs($inheritPosts);
 		echo '<div class="buttonDiv">';
 		printUpdateButton('keyword');
 		$keyword['keyword']=$keyword['keyword']['keyword_id'];
 		printInfoButton($keyword);
-		echo '</div></form></div>';
 		$deleteConfirmStr="Är du säker att du vill radera nyckelordet ".$keyword['keyword']."? Referenser till nyckelordet hanteras separat.";
 		printDeleteButton($keyword, $deleteConfirmStr, $inheritPosts);
-		echo '</div>';
+		echo '</div></form></div></div>';
 	}
 
 ?>
