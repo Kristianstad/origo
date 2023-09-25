83314a24c717e45bfb07f9373f70a892d432ad59
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-09-25 11:48:27 +0200

    Update printTableForm.php

--- finalfs/origo/adm/functions/manage/printTableForm.php
+++ finalfs/origo/adm/functions/manage/printTableForm.php
@@ -1,32 +1,36 @@
 <?php
 
+	require_once("./functions/manage/isFullTarget.php");
 	require_once("./functions/manage/printTextarea.php");
 	require_once("./functions/manage/printHiddenInputs.php");
 	require_once("./functions/manage/printUpdateButton.php");
 	require_once("./functions/manage/printInfoButton.php");
 	require_once("./functions/manage/printDeleteButton.php");
 
-	function printTableForm($table, $selectables, $inheritPosts)
+	function printTableForm($table, $selectables, $inheritPosts, $helps=array())
 	{
+		if (!isFullTarget($table))
+		{
+			die("printTableForm($table, $selectables, $inheritPosts, $helps=array()) failed!");
+		}
 		echo '<div><div class="printXFormDiv"><form method="post">';
-		printTextarea($table, 'table_id', 'textareaMedium', 'Id:');
-		printTextarea($table, 'abstract', 'textareaLarge', 'Beskrivning:');
-		printTextarea($table, 'keywords', 'textareaLarge', 'Nyckelord:');
-		printUpdateSelect($table, array('contact'=>$selectables['contacts']), 'bodySelect', 'Kontakt:');
-		printUpdateSelect($table, array('origin'=>$selectables['origins']), 'bodySelect', 'Ursprungskälla:');
-		printTextarea($table, 'updated', 'textareaMedium', 'Uppdaterad (åååå-mm-dd):');
-		printUpdateSelect($table, array('update'=>$selectables['updates']), 'bodySelect', 'Uppdatering:');
-		printTextarea($table, 'history', 'textareaLarge', 'Tillkomsthistorik:');
-		printTextarea($table, 'info', 'textareaLarge', 'Info:');
+		printTextarea($table, 'table_id', 'textareaMedium', 'Id:', in_array('table_id', $helps));
+		printTextarea($table, 'abstract', 'textareaLarge', 'Beskrivning:', in_array('abstract', $helps));
+		printTextarea($table, 'keywords', 'textareaLarge', 'Nyckelord:', in_array('keywords', $helps));
+		printUpdateSelect($table, array('contact'=>$selectables['contacts']), 'bodySelect', 'Kontakt:', in_array('contact', $helps));
+		printUpdateSelect($table, array('origin'=>$selectables['origins']), 'bodySelect', 'Ursprungskälla:', in_array('origin', $helps));
+		printTextarea($table, 'updated', 'textareaMedium', 'Uppdaterad (åååå-mm-dd):', in_array('updated', $helps));
+		printUpdateSelect($table, array('update'=>$selectables['updates']), 'bodySelect', 'Uppdatering:', in_array('update', $helps));
+		printTextarea($table, 'history', 'textareaLarge', 'Tillkomsthistorik:', in_array('history', $helps));
+		printTextarea($table, 'info', 'textareaLarge', 'Info:', in_array('info', $helps));
 		printHiddenInputs($inheritPosts);
 		echo '<div class="buttonDiv">';
 		printUpdateButton('table');
 		$table['table']=$table['table']['table_id'];
 		printInfoButton($table);
-		echo '</div></form></div>';
 		$deleteConfirmStr="Är du säker att du vill radera all metadata för tabellen ".$table['table']."?";
 		printDeleteButton($table, $deleteConfirmStr, $inheritPosts);
-		echo '</div>';
+		echo '</div></form></div></div>';
 	}
 
 ?>
