789ae1d4ea43c563ba10a6e0f7fe1690a5a291f4
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-09-25 12:03:34 +0200

    Update toggleTopFrame.js

--- finalfs/origo/adm/js-functions/manage/toggleTopFrame.js
+++ finalfs/origo/adm/js-functions/manage/toggleTopFrame.js
@@ -4,10 +4,15 @@ function toggleTopFrame(type)
 	if (x.style.display === "none")
 	{
 		x.style.display = "block";
+		window.scrollTo(0, 0);
 	}
 	else if (topFrame === type)
 	{
 		x.style.display = "none";
+	}
+	else
+	{
+		window.scrollTo(0, 0);
 	}
 	topFrame = type;
 }
