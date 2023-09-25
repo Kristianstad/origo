1cadae91b6f3a3d9ed1a8b2c9fdd8727753701e4
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-09-25 12:01:36 +0200

    Update printContactForm.php

--- finalfs/origo/adm/functions/manage/printContactForm.php
+++ finalfs/origo/adm/functions/manage/printContactForm.php
@@ -1,29 +1,33 @@
 <?php
 
+	require_once("./functions/manage/isFullTarget.php");
 	require_once("./functions/manage/printTextarea.php");
 	require_once("./functions/manage/printHiddenInputs.php");
 	require_once("./functions/manage/printUpdateButton.php");
 	require_once("./functions/manage/printInfoButton.php");
 	require_once("./functions/manage/printDeleteButton.php");
 
-	function printContactForm($contact, $inheritPosts)
+	function printContactForm($contact, $inheritPosts, $helps=array())
 	{
+		if (!isFullTarget($contact))
+		{
+			die("printContactForm($contact, $inheritPosts, $helps=array()) failed!");
+		}
 		echo '<div><div class="printXFormDiv"><form method="post">';
-		printTextarea($contact, 'contact_id', 'textareaMedium', 'Id:');
-		printTextarea($contact, 'name', 'textareaMedium', 'Namn:');
-		printTextarea($contact, 'web', 'textareaMedium', 'Webbsida:');
-		printTextarea($contact, 'email', 'textareaMedium', 'E-mail:');
-		printTextarea($contact, 'abstract', 'textareaLarge', 'Beskrivning:');
-		printTextarea($contact, 'info', 'textareaLarge', 'Info:');
+		printTextarea($contact, 'contact_id', 'textareaMedium', 'Id:', in_array('contact_id', $helps));
+		printTextarea($contact, 'name', 'textareaMedium', 'Namn:', in_array('name', $helps));
+		printTextarea($contact, 'web', 'textareaMedium', 'Webbsida:', in_array('web', $helps));
+		printTextarea($contact, 'email', 'textareaMedium', 'E-mail:', in_array('email', $helps));
+		printTextarea($contact, 'abstract', 'textareaLarge', 'Beskrivning:', in_array('abstract', $helps));
+		printTextarea($contact, 'info', 'textareaLarge', 'Info:', in_array('info', $helps));
 		printHiddenInputs($inheritPosts);
 		echo '<div class="buttonDiv">';
 		printUpdateButton('contact');
 		$contact['contact']=$contact['contact']['contact_id'];
 		printInfoButton($contact);
-		echo '</div></form></div>';
 		$deleteConfirmStr="Är du säker att du vill radera kontakten ".$contact['contact']."? Referenser till kontakten hanteras separat.";
 		printDeleteButton($contact, $deleteConfirmStr, $inheritPosts);
-		echo '</div>';
+		echo '</div></form></div></div>';
 	}
-	
+
 ?>
