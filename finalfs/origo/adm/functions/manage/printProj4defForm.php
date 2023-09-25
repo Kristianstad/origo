a35be4c3d9c5da2fdf7e2def718aa45f2313b390
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-09-25 11:51:52 +0200

    Update printProj4defForm.php

--- finalfs/origo/adm/functions/manage/printProj4defForm.php
+++ finalfs/origo/adm/functions/manage/printProj4defForm.php
@@ -1,28 +1,32 @@
 <?php
 
+	require_once("./functions/manage/isFullTarget.php");
 	require_once("./functions/manage/printTextarea.php");
 	require_once("./functions/manage/printHiddenInputs.php");
 	require_once("./functions/manage/printUpdateButton.php");
 	require_once("./functions/manage/printInfoButton.php");
 	require_once("./functions/manage/printDeleteButton.php");
 
-	function printProj4defForm($proj4def, $inheritPosts)
+	function printProj4defForm($proj4def, $inheritPosts, $helps=array())
 	{
+		if (!isFullTarget($proj4def))
+		{
+			die("printProj4defForm($proj4def, $inheritPosts, $helps=array()) failed!");
+		}
 		echo '<div><div class="printXFormDiv"><form method="post">';
-		printTextarea($proj4def, 'code', 'textareaMedium', 'Kod:');
-		printTextarea($proj4def, 'projection', 'textareaLarge', 'Projektion:');
-		printTextarea($proj4def, 'alias', 'textareaMedium', 'Alias:');
-		printTextarea($proj4def, 'abstract', 'textareaLarge', 'Beskrivning:');
-		printTextarea($proj4def, 'info', 'textareaLarge', 'Info:');
+		printTextarea($proj4def, 'code', 'textareaMedium', 'Kod:', in_array('code', $helps));
+		printTextarea($proj4def, 'projection', 'textareaLarge', 'Projektion:', in_array('projection', $helps));
+		printTextarea($proj4def, 'alias', 'textareaMedium', 'Alias:', in_array('alias', $helps));
+		printTextarea($proj4def, 'abstract', 'textareaLarge', 'Beskrivning:', in_array('abstract', $helps));
+		printTextarea($proj4def, 'info', 'textareaLarge', 'Info:', in_array('info', $helps));
 		printHiddenInputs($inheritPosts);
 		echo '<div class="buttonDiv">';
 		printUpdateButton('proj4def');
 		$proj4def['proj4def']=$proj4def['proj4def']['code'];
 		printInfoButton($proj4def);
-		echo '</div></form></div>';
 		$deleteConfirmStr="Är du säker att du vill radera proj4def ".$proj4def['proj4def']."? Referenser till aktuell proj4def hanteras separat.";
 		printDeleteButton($proj4def, $deleteConfirmStr, $inheritPosts);
-		echo '</div>';
+		echo '</div></form></div></div>';
 	}
-	
+
 ?>
