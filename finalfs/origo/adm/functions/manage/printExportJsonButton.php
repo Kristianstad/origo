15844ee81f23b9c99597c2f282da741fd8dd3923
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-10-20 13:53:23 +0200

    Update printExportJsonButton.php

--- finalfs/origo/adm/functions/manage/printExportJsonButton.php
+++ finalfs/origo/adm/functions/manage/printExportJsonButton.php
@@ -5,6 +5,7 @@
 		echo <<<HERE
 			<form action="writeConfig.php" method="get" target="hiddenFrame">
 				<input type="hidden" name="getJson" value="y">
+				<input type="hidden" name="download" value="y">
 				<button class="exportButton" type="submit" name="map" value="{$mapId}">
 					Exportera JSON
 				</button>
