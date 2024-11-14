24f1a06a7cefdd26a27a222583423b36e336993b
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-14 12:13:00 +0100

    Update targetTable.php

--- finalfs/www/adm/functions/manage/targetTable.php
+++ finalfs/www/adm/functions/manage/targetTable.php
@@ -2,13 +2,13 @@
 
 	require_once("./functions/manage/isTarget.php");
 	require_once("./functions/manage/targetType.php");
-	require_once("./functions/manage/typeTable.php");
+	require_once("./functions/manage/typeTableName.php");
 
 	function targetTable($target)
 	{
 		if (isTarget($target))
 		{
-			return typeTable(targetType($target));
+			return typeTableName(targetType($target));
 		}
 		else
 		{
