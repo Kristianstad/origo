a08455c2faa2b0f24cb53bab9bdafc6276aeb7fd
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-20 13:48:37 +0100

    Update updatePosts.php

--- finalfs/www/adm/functions/manage/updatePosts.php
+++ finalfs/www/adm/functions/manage/updatePosts.php
@@ -1,5 +1,6 @@
 <?php
 
+	// Takes an associative array and returns all values (and keys) where the key ends with 'update'
 	function updatePosts($post)
 	{
 		return array_filter($post, function($key) {return (substr($key, 0, 6) == 'update');}, ARRAY_FILTER_USE_KEY);
