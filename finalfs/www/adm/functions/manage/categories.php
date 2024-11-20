a2e7891387ecc72d011d1fa38467f056355994d3
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-20 12:08:24 +0100

    Update categories.php

--- finalfs/www/adm/functions/manage/categories.php
+++ finalfs/www/adm/functions/manage/categories.php
@@ -2,6 +2,9 @@
 
 	require_once("./functions/pgArrayToPhp.php");
 
+	// Takes an Origo table configuration and the primary key- (id-) column name for the same table. 
+	// Returns an associative array with the keyword categories for the specific table. 
+	// The array keys are keywords and the values are arrays of item ids that has the corresponding keyword set in their config
 	function categories($config, $catParam)
 	{
 		$categories=array();
