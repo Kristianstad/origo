6a442c1085e7773f4443d3c9f4b749b185ba6c5e
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-20 13:43:40 +0100

    Update deleteIdSql.php

--- finalfs/www/adm/functions/manage/deleteIdSql.php
+++ finalfs/www/adm/functions/manage/deleteIdSql.php
@@ -2,6 +2,8 @@
 
 	require_once("./functions/pkColumnOfTable.php");
 
+	// Takes an id and a table name as parameters.
+	// Returns a sql-query string for deleting the row in the table that has given id as primary key
 	function deleteIdSql($id, $tableName)
 	{
 		require("./constants/configSchema.php");
