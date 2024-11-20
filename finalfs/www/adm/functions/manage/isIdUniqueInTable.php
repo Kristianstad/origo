3bc183785f4dcc4aec66d403eae8d237a379b69c
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-20 13:34:10 +0100

    Update isIdUniqueInTable.php

--- finalfs/www/adm/functions/manage/isIdUniqueInTable.php
+++ finalfs/www/adm/functions/manage/isIdUniqueInTable.php
@@ -1,5 +1,7 @@
 <?php
 
+	// Takes an id, the primary key- (id-) column, and a table (config) as parameters
+	// Returns true if the given id is unique in the table, else returns false
 	function isIdUniqueInTable($id, $tablePkColumn, $table)
 	{
 		return !in_array($id, array_column($table, $tablePkColumn));
