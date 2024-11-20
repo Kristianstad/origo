80ce69009786ebb92a1822727d7e51620dde2215
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-20 10:32:06 +0100

    Update idPosts.php

--- finalfs/www/adm/functions/manage/idPosts.php
+++ finalfs/www/adm/functions/manage/idPosts.php
@@ -1,6 +1,6 @@
 <?php
 
-	// Takes an associative array and returns all values (and keys) where the key ends with Id, with the exception of following keys: fromMapId, toMapId, fromGroupId, toGroupId
+	// Takes an associative array and returns all values (and keys) where the key ends with 'Id', with the exception of following keys: 'fromMapId', 'toMapId', 'fromGroupId', 'toGroupId'
 	function idPosts($post)
 	{
 		$idPosts=array_filter($post, function($key) {return (substr($key, -2) == 'Id');}, ARRAY_FILTER_USE_KEY);
