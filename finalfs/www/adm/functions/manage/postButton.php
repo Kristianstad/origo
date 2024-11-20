c50504f8f44386177232ae43ac34cf55fdf0eff0
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-20 12:15:00 +0100

    Update postButton.php

--- finalfs/www/adm/functions/manage/postButton.php
+++ finalfs/www/adm/functions/manage/postButton.php
@@ -1,5 +1,6 @@
 <?php
 
+	// Takes an associative array and returns the first value where the key ends with 'Button' (should only be one)
 	function postButton($post)
 	{
 		$postButton=array_keys(array_filter($post, function($key) {return (substr($key, -6) == 'Button');}, ARRAY_FILTER_USE_KEY));
