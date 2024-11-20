c10191431cf697ae8b8bc0c3b18cb25bc28ef289
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-20 13:40:15 +0100

    Update insertIdSql.php

--- finalfs/www/adm/functions/manage/insertIdSql.php
+++ finalfs/www/adm/functions/manage/insertIdSql.php
@@ -2,6 +2,8 @@
 
 	require_once("./functions/pkColumnOfTable.php");
 
+	// Takes an id and a table name as parameters.
+	// Returns a sql-query string for creating a new row in the table with given id as primary key
 	function insertIdSql($id, $tableName)
 	{
 		require("./constants/configSchema.php");
