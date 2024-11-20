f0d5d062b5e32736baee2d53e8886ce1c3e82858
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-20 11:31:06 +0100

    Update viewKeywordCategorized.php

--- finalfs/www/adm/functions/manage/viewKeywordCategorized.php
+++ finalfs/www/adm/functions/manage/viewKeywordCategorized.php
@@ -1,5 +1,9 @@
 <?php
 
+	// Takes a view (string) as parameter. If the given view is an empty string or 'Allt', 
+	// then return the constant in keywordCategorized.php. Else, read the constant in views.php
+	// to determine which tables should be shown in the manager, then return a subset of the
+	// constant in keywordCategorized.php reflecting that.
 	function viewKeywordCategorized($view)
 	{
 		require("./constants/views.php");
