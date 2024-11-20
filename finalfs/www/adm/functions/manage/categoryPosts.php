14ec6105d9e4c8c15f8ba7759c3b3935ef33da2e
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-20 10:34:32 +0100

    Update categoryPosts.php

--- finalfs/www/adm/functions/manage/categoryPosts.php
+++ finalfs/www/adm/functions/manage/categoryPosts.php
@@ -1,5 +1,6 @@
 <?php
 
+	// Takes an associative array and returns all values (and keys) where the key ends with 'Category'
 	function categoryPosts($post)
 	{
 		$categoryPosts=array_filter($post, function($key) {return (substr($key, -8) == 'Category');}, ARRAY_FILTER_USE_KEY);
