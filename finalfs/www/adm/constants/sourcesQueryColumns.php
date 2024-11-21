f27c18a1dae52c582d0f62f7a64caec212c783ed
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-21 11:18:53 +0100

    Update sourcesQueryColumns.php

--- finalfs/www/adm/constants/sourcesQueryColumns.php
+++ finalfs/www/adm/constants/sourcesQueryColumns.php
@@ -1,5 +1,6 @@
 <?php
 
+	// An array of column names present in the sources configuration table that are url query parameters that writeConfig.php should add to the base_url of the source
 	$sourcesQueryColumns=array('with_geometry', 'fi_point_tolerance', 'ttl');
 
 ?>
