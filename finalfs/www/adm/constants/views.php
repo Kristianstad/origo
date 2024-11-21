ac76ee8b19ff7dc4ad3797b45185ef2b75b85d53
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-21 11:33:51 +0100

    Update views.php

--- finalfs/www/adm/constants/views.php
+++ finalfs/www/adm/constants/views.php
@@ -1,5 +1,7 @@
 <?php
 
+	// An associative array of the different views that the manage tool can switch between. 
+	// Each key contains a views name and the corresponding value contains an array of names of configuration tables that should be displayed by the tool
 	$views = array(
 		'Allt'	=> array(),
 		'Origo' => array('maps', 'controls', 'groups', 'layers', 'sources', 'services', 'tilegrids', 'footers', 'proj4defs'),
