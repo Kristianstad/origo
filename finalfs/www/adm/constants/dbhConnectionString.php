965501b03073873396a5b3ffa8cb84ad4e923dd9
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-21 10:31:42 +0100

    Update dbhConnectionString.php

--- finalfs/www/adm/constants/dbhConnectionString.php
+++ finalfs/www/adm/constants/dbhConnectionString.php
@@ -1,5 +1,6 @@
 <?php
 
+	// The connection string used by pg_connect to create database handles for the Origo configuration database
 	$dbhConnectionString = 'host=localhost port=5432 dbname=origo user=postgres password=postgres' ;
 
 ?>
