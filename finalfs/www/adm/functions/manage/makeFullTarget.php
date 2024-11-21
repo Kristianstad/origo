fc5dde48d15ab05d31aff9ac3a158cb9ab1ebf7b
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-21 13:27:37 +0100

    Update makeFullTarget.php

--- finalfs/www/adm/functions/manage/makeFullTarget.php
+++ finalfs/www/adm/functions/manage/makeFullTarget.php
@@ -1,5 +1,7 @@
 <?php
 
+	// Takes a type string and config array and combine them into a full target array.
+	// The full target array has the type as only key and config array as value
 	function makeFullTarget($type, $config)
 	{
 		if (is_string($type) && !empty($type) && is_array($config) && !empty($config))
