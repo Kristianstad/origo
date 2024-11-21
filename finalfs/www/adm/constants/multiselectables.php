5d2e3fd06645091dc877c1ac809a53bfbe559590
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-21 10:55:17 +0100

    Update multiselectables.php

--- finalfs/www/adm/constants/multiselectables.php
+++ finalfs/www/adm/constants/multiselectables.php
@@ -1,5 +1,7 @@
 <?php
 
+	// An array of names of configuration tables and columns that should have a multiselect button in the manage tool
+	// (they also need to be present in arrayColumns.php)
 	$multiselectables=array('controls', 'groups', 'layers', 'proj4defs', 'keywords');
 
 ?>
