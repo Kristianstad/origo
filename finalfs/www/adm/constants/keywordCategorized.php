c703324f95e7d7a371c175ccae786af46b815578
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-21 10:41:30 +0100

    Update keywordCategorized.php

--- finalfs/www/adm/constants/keywordCategorized.php
+++ finalfs/www/adm/constants/keywordCategorized.php
@@ -1,5 +1,6 @@
 <?php
 
+	// Origo configuration tables that has a keywords column and should be categorized by these keywords in the manage tool
 	$keywordCategorized=array('groups', 'layers', 'tables', 'schemas');
 
 ?>
