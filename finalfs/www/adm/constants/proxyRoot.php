4456289547624d7d3857c5b5670e88dedb6d8f42
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-21 11:35:57 +0100

    Update proxyRoot.php

--- finalfs/www/adm/constants/proxyRoot.php
+++ finalfs/www/adm/constants/proxyRoot.php
@@ -1,6 +1,6 @@
 <?php
 
-	// Additional path (string) that is prepended to generated urls. Needed if Origo is located behind a proxy
+	// Additional path (string) that is prepended to generated Origo map urls. Needed if Origo is located behind a proxy
 	$proxyRoot = '';
 
 ?>
