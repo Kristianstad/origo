f2581be506c473006a2bb573a8bc6e175588e02b
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-22 12:55:24 +0100

    Update makeBasicTarget.php

--- finalfs/www/adm/functions/manage/makeBasicTarget.php
+++ finalfs/www/adm/functions/manage/makeBasicTarget.php
@@ -1,5 +1,7 @@
 <?php
 
+	// Takes a type string and an id string and combine them into a basic target array.
+	// The basic target array has the type as only key and the id as value
 	function makeBasicTarget($type, $id)
 	{
 		if (is_string($type) && !empty($type) && is_string($id) && !empty($id))
