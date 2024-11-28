f440b2b386aabcc338a62881b5dae78c572ec753
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-28 14:00:05 +0100

    Update array_column_search.php

--- finalfs/www/adm/functions/array_column_search.php
+++ finalfs/www/adm/functions/array_column_search.php
@@ -1,5 +1,9 @@
 <?php
 
+	// Takes a search (string, array), a column (string), an array, and optionally a return key (string) as parameters.
+	// Searches the specified column of the array for the given search. Returns an empty array if no match is found.
+	// If a match is found and no return key was given, then it returns the the first matching value.
+	// If a return key was given then it is assumed that the matching value is an array and returns the value of its return key (match[returnkey]).
 	function array_column_search($search, $column, $array, $return=false)
 	{
 		$columnValues = array_column($array, $column);
@@ -20,5 +24,5 @@
 			return array();
 		}
 	}
-	
+
 ?>
