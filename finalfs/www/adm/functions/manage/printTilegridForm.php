d80cb0d94a523005dc5a7ce64b0e8886a00765da
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-29 11:51:53 +0100

    Update printTilegridForm.php

--- finalfs/www/adm/functions/manage/printTilegridForm.php
+++ finalfs/www/adm/functions/manage/printTilegridForm.php
@@ -7,6 +7,8 @@
 	require_once("./functions/manage/printInfoButton.php");
 	require_once("./functions/manage/printDeleteButton.php");
 
+	// Takes a full tilegrid target (array), inheritPosts (array), and helps (array).
+	// Prints form fields and buttons that are used to view and edit the configuration for the given tilegrid.
 	function printTilegridForm($tilegrid, $inheritPosts, $helps=array())
 	{
 		if (!isFullTarget($tilegrid))
