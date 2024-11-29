8e0a13bfcd576dfd9e88becd35483a7222c32dd7
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-29 10:46:05 +0100

    Update targetConfigParam.php

--- finalfs/www/adm/functions/manage/targetConfigParam.php
+++ finalfs/www/adm/functions/manage/targetConfigParam.php
@@ -2,6 +2,8 @@
 
 	require_once("./functions/manage/isFullTarget.php");
 
+	// Takes a full target array and an Origo configuration parameter name (string).
+	// Returns the value for the given configuration parameter for the given target
 	function targetConfigParam($fullTarget, $configParam)
 	{
 		if (isFullTarget($fullTarget))
