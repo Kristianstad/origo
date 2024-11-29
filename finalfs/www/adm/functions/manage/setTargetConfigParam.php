b4a3ac8c4a7fe3ecfc5e86d06338c965e90491a9
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-29 10:50:54 +0100

    Update setTargetConfigParam.php

--- finalfs/www/adm/functions/manage/setTargetConfigParam.php
+++ finalfs/www/adm/functions/manage/setTargetConfigParam.php
@@ -2,6 +2,8 @@
 
 	require_once("./functions/manage/isFullTarget.php");
 
+	// Takes a full target array, an Origo configuration parameter name (string), and a configuration parameter value.
+	// Sets the configuration parameter to the new value in the given target.
 	function setTargetConfigParam(&$fullTarget, $configParam, $value)
 	{
 		if (isFullTarget($fullTarget))
