6d4f3eb0c67116218e201ebadd61804451ddfd55
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-29 10:23:03 +0100

    Update makeTargetFull.php

--- finalfs/www/adm/functions/manage/makeTargetFull.php
+++ finalfs/www/adm/functions/manage/makeTargetFull.php
@@ -4,6 +4,8 @@
 	require_once("./functions/manage/targetType.php");
 	require_once("./functions/manage/targetConfig.php");
 
+	// Takes a basic target array as first parameter and configTables (array) or database handle as second parameter.
+	// Returns a full target array
 	function makeTargetFull($target, $configTablesOrDbh)
 	{
 		if (isTarget($target))
