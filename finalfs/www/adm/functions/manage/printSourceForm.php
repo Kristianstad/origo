bfdfa5c1acea6ac362a48cdb15e8e2a4765dd270
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-11-29 11:00:55 +0100

    Update printSourceForm.php

--- finalfs/www/adm/functions/manage/printSourceForm.php
+++ finalfs/www/adm/functions/manage/printSourceForm.php
@@ -8,6 +8,8 @@
 	require_once("./functions/manage/printInfoButton.php");
 	require_once("./functions/manage/printDeleteButton.php");
 
+	// Takes a full source target (array), source selectables (array), inheritPosts (array), and helps (array).
+	// Prints form fields and buttons that are used to view and edit the configuration for the given source.
 	function printSourceForm($source, $selectables, $inheritPosts, $helps=array())
 	{
 		if (!isFullTarget($source))
