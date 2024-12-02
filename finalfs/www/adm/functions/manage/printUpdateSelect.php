a5714eac24525e5eabe3faffaadb4022a7da284d
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-12-02 14:07:06 +0100

    Update printUpdateSelect.php

--- finalfs/www/adm/functions/manage/printUpdateSelect.php
+++ finalfs/www/adm/functions/manage/printUpdateSelect.php
@@ -5,7 +5,7 @@
 	require_once("./functions/manage/printSelectOptions.php");
 	require_once("./functions/manage/targetType.php");
 
-	// Takes a full target array, parameter values (array), style class (string), label (string), help-exist (boolean), select options (optional, array, needs to have equal number of elements as parameter two).
+	// Takes a full target array, parameter values (array), style class (string), label (string), help-exist (boolean), and select options (optional, array, needs to have equal number of elements as parameter two).
 	// Prints a drop-down selection box with option values to choose from. It's the parameter value of the chosen option that is set to be posted on form submit.
 	// Parameter three and four are used to set the class and label for the select tag. A help-button is printed if a help exists.
 	function printUpdateSelect($fullTarget, $configParamValues, $class, $label, $help=false, $options=null)
