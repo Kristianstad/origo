83759ededecb4084389c74be3ef4a3c178ce637a
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-12-09 12:46:54 +0100

    Update printServiceForm.php

--- finalfs/www/adm/functions/manage/printServiceForm.php
+++ finalfs/www/adm/functions/manage/printServiceForm.php
@@ -24,7 +24,7 @@
 		printHiddenInputs($inheritPosts);
 		echo '<div class="buttonDiv">';
 		printUpdateButton('service');
-		$service['service']=$service['service']['service_id'];
+		$service=makeTargetBasic($service);
 		printInfoButton($service);
 		$deleteConfirmStr="Är du säker att du vill radera tjänsten ".$service['service']."? Referenser till tjänsten hanteras separat.";
 		printDeleteButton($service, $deleteConfirmStr, $inheritPosts);
