f76be914a410b7d7c189747d5e3c87fd4859beca
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-12-09 12:19:49 +0100

    Update isBasicTarget.php

--- finalfs/www/adm/functions/manage/isBasicTarget.php
+++ finalfs/www/adm/functions/manage/isBasicTarget.php
@@ -2,6 +2,7 @@
 
 	require_once("./functions/manage/isTarget.php");
 
+	// Takes a target array and returns true if it is a basic target array. Returns false if it is a full target array.
 	function isBasicTarget($target)
 	{
 		return (isTarget($target) && is_string(current($target)));
