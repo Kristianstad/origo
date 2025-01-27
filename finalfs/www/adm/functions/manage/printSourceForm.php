f11a2da0d0d28d6a33e47f31e5af2ac64709f7c1
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-01-27 13:15:08 +0100

    Update printSourceForm.php

--- finalfs/www/adm/functions/manage/printSourceForm.php
+++ finalfs/www/adm/functions/manage/printSourceForm.php
@@ -1,6 +1,7 @@
 <?php
 
 	require_once("./functions/manage/isFullTarget.php");
+	require_once("./functions/manage/targetConfigParam.php");
 	require_once("./functions/manage/printTextarea.php");
 	require_once("./functions/manage/printUpdateSelect.php");
 	require_once("./functions/manage/printHiddenInputs.php");
@@ -19,20 +20,31 @@
 		echo '<div><div class="printXFormDiv"><form method="post">';
 		printTextarea($source, 'source_id', 'textareaMedium', 'Id:', in_array('source_id', $helps));
 		printUpdateSelect($source, array('service'=>$selectables['services']), 'bodySelect', 'Tjänst:', in_array('service', $helps));
-		printUpdateSelect($source, array('with_geometry'=>array("f", "t")), 'miniSelect', 'With_geometry:', in_array('with_geometry', $helps));
-		printTextarea($source, 'fi_point_tolerance', 'textareaSmall', 'Fi_point_tolerance:', in_array('fi_point_tolerance', $helps));
-		printTextarea($source, 'ttl', 'textareaSmall', 'Ttl:', in_array('ttl', $helps));
-		printUpdateSelect($source, array('tilegrid'=>$selectables['tilegrids']), 'bodySelect', 'Tilegrid:', in_array('tilegrid', $helps));
-		printTextarea($source, 'softversion', 'textareaSmall', 'Programversion:', in_array('softversion', $helps));
-		printTextarea($source, 'abstract', 'textareaLarge', 'Beskrivning:', in_array('abstract', $helps));
-		printUpdateSelect($source, array('contact'=>$selectables['contacts']), 'bodySelect', 'Kontakt:', in_array('contact', $helps));
-		printTextarea($source, 'updated', 'textareaMedium', 'Uppdaterad (åååå-mm-dd):', in_array('updated', $helps));
-		printTextarea($source, 'history', 'textareaLarge', 'Tillkomsthistorik:', in_array('history', $helps));
-		printTextarea($source, 'info', 'textareaLarge', 'Info:', in_array('info', $helps));
-		if (isset(current($source)['tables']) && !empty(trim(current($source)['tables'], '{}')))
+		$sourceServiceId=targetConfigParam($source, 'service');
+		if (!empty($sourceServiceId))
 		{
-			printTextarea($source, 'tables', 'textareaLarge', 'Tabeller:', 'yes');
+			if ($sourceServiceId == "GEOJSON")
+			{
+				printTextarea($source, 'file', 'textareaLarge', 'Fil:', in_array('file', $helps));
+			}
+			else
+			{
+				printUpdateSelect($source, array('with_geometry'=>array("f", "t")), 'miniSelect', 'With_geometry:', in_array('with_geometry', $helps));
+				printTextarea($source, 'fi_point_tolerance', 'textareaSmall', 'Fi_point_tolerance:', in_array('fi_point_tolerance', $helps));
+				printTextarea($source, 'ttl', 'textareaSmall', 'Ttl:', in_array('ttl', $helps));
+				printUpdateSelect($source, array('tilegrid'=>$selectables['tilegrids']), 'bodySelect', 'Tilegrid:', in_array('tilegrid', $helps));
+				printTextarea($source, 'softversion', 'textareaSmall', 'Programversion:', in_array('softversion', $helps));
+				if (isset(current($source)['tables']) && !empty(trim(current($source)['tables'], '{}')))
+				{
+					printTextarea($source, 'tables', 'textareaLarge', 'Tabeller:', 'yes');
+				}
+			}
+			printTextarea($source, 'abstract', 'textareaLarge', 'Beskrivning:', in_array('abstract', $helps));
+			printUpdateSelect($source, array('contact'=>$selectables['contacts']), 'bodySelect', 'Kontakt:', in_array('contact', $helps));
+			printTextarea($source, 'updated', 'textareaMedium', 'Uppdaterad (åååå-mm-dd):', in_array('updated', $helps));
+			printTextarea($source, 'history', 'textareaLarge', 'Tillkomsthistorik:', in_array('history', $helps));
 		}
+		printTextarea($source, 'info', 'textareaLarge', 'Info:', in_array('info', $helps));
 		printHiddenInputs($inheritPosts);
 		echo '<div class="buttonDiv">';
 		printUpdateButton('source');
