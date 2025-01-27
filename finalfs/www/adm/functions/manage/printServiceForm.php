163e1d8adb4009bb8cf5366e720da35a90d7d020
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-01-27 14:00:50 +0100

    Update printServiceForm.php

--- finalfs/www/adm/functions/manage/printServiceForm.php
+++ finalfs/www/adm/functions/manage/printServiceForm.php
@@ -2,6 +2,7 @@
 
 	require_once("./functions/manage/isFullTarget.php");
 	require_once("./functions/manage/printTextarea.php");
+	require_once("./functions/manage/targetConfigParam.php");
 	require_once("./functions/manage/printHiddenInputs.php");
 	require_once("./functions/manage/printUpdateButton.php");
 	require_once("./functions/manage/printInfoButton.php");
@@ -17,10 +18,17 @@
 		}
 		echo '<div><div class="printXFormDiv"><form method="post">';
 		printTextarea($service, 'service_id', 'textareaMedium', 'Id:', in_array('service_id', $helps));
-		printTextarea($service, 'base_url', 'textareaLarge', 'Huvudurl:', in_array('base_url', $helps));
 		printUpdateSelect($service, array('type'=>array("File","QGIS","Geoserver")), 'bodySelect', 'Typ:', in_array('type', $helps));
-		printTextarea($service, 'formats', 'textareaLarge', 'Tillgängliga format:', in_array('formats', $helps));
-		printTextarea($service, 'abstract', 'textareaLarge', 'Beskrivning:', in_array('abstract', $helps));
+		$serviceType=targetConfigParam($service, 'type');
+		if (!empty($serviceType))
+		{
+			if ($serviceType != 'File')
+			{
+				printTextarea($service, 'base_url', 'textareaLarge', 'Huvudurl:', in_array('base_url', $helps));
+			}
+			printTextarea($service, 'formats', 'textareaLarge', 'Tillgängliga format:', in_array('formats', $helps));
+			printTextarea($service, 'abstract', 'textareaLarge', 'Beskrivning:', in_array('abstract', $helps));
+		}
 		printTextarea($service, 'info', 'textareaLarge', 'Info:', in_array('info', $helps));
 		printHiddenInputs($inheritPosts);
 		echo '<div class="buttonDiv">';
