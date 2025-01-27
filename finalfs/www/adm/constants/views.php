560c3978340326dff89bdc16ed603a7cb34cb8d6
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-01-27 12:50:11 +0100

    Update views.php

--- finalfs/www/adm/constants/views.php
+++ finalfs/www/adm/constants/views.php
@@ -4,7 +4,7 @@
 	// Each key contains a views name and the corresponding value contains an array of names of configuration tables that should be displayed by the tool
 	$views = array(
 		'Allt'	=> array(),
-		'Origo' => array('maps', 'controls', 'groups', 'layers', 'sources', 'services', 'tilegrids', 'footers', 'proj4defs'),
+		'Origo' => array('maps', 'controls', 'groups', 'layers', 'sources', 'services', 'tilegrids', 'footers', 'proj4defs', 'formats'),
 		'Meta'	=> array('databases', 'schemas', 'tables', 'contacts', 'origins', 'updates', 'keywords'),
 		'Verktyg' => array('helps')
 	);
