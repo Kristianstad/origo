9298cabe96ad640884e9330bbe49e94e98c79d54
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-01-27 12:53:30 +0100

    Create printFormatForm.php

---
+++ finalfs/www/adm/functions/manage/printFormatForm.php
@@ -0,0 +1,32 @@
+<?php
+
+	require_once("./functions/manage/isFullTarget.php");
+	require_once("./functions/manage/printTextarea.php");
+	require_once("./functions/manage/printHiddenInputs.php");
+	require_once("./functions/manage/printUpdateButton.php");
+	require_once("./functions/manage/printInfoButton.php");
+	require_once("./functions/manage/printDeleteButton.php");
+
+	// Takes a full format target (array), inheritPosts (array), and helps (array).
+	// Prints form fields and buttons that are used to view and edit the configuration for the given format.
+	function printFormatForm($format, $inheritPosts, $helps=array())
+	{
+		if (!isFullTarget($format))
+		{
+			die("printFormatForm($format, $inheritPosts, $helps=array()) failed!");
+		}
+		echo '<div><div class="printXFormDiv"><form method="post">';
+		printTextarea($format, 'format_id', 'textareaMedium', 'Format:', in_array('format_id', $helps));
+		printTextarea($format, 'abstract', 'textareaLarge', 'Beskrivning:', in_array('abstract', $helps));
+		printTextarea($format, 'info', 'textareaLarge', 'Info:', in_array('info', $helps));
+		printHiddenInputs($inheritPosts);
+		echo '<div class="buttonDiv">';
+		printUpdateButton('format');
+		$format=makeTargetBasic($format);
+		printInfoButton($format);
+		$deleteConfirmStr="Är du säker att du vill radera formatet ".$format['format']."? Referenser till formatet hanteras separat.";
+		printDeleteButton($format, $deleteConfirmStr, $inheritPosts);
+		echo '</div></form></div></div>';
+	}
+
+?>
