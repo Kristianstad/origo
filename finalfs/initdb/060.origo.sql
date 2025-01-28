4bc19afc9ea5646b3bad6293996fedb1344541ba
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-01-28 14:43:25 +0100

    Update 060.origo.sql

--- finalfs/initdb/060.origo.sql
+++ finalfs/initdb/060.origo.sql
@@ -144,7 +144,7 @@ CREATE TABLE map_configs.maps
     CONSTRAINT map_pk PRIMARY KEY (map_id)
 );
 
-INSERT INTO map_configs.maps(map_id,title,footer,layers,groups,abstract,show_meta) VALUES ('Origostäder','Demokarta - Origo','origo#1','{origo-cities#1}','{none#1,background#1}','En demokarta som visar kommuner delaktiga i Origoprojektet.',true);
+INSERT INTO map_configs.maps(map_id,title,footer,layers,groups,abstract,show_meta) VALUES ('demokarta','Demokarta - Origo','origo#1','{origo-cities#1}','{none#1,background#1}','En demokarta som visar kommuner delaktiga i Origoprojektet.',true);
 INSERT INTO map_configs.maps(map_id,title,footer,groups,abstract,show_meta) VALUES ('preview','Förhandsgranska - Origo','origo#1','{background#1}','En karta som används för att visa förhandsgranskningar i administrationsverktyget.',true);
 
 CREATE TABLE map_configs.proj4defs
