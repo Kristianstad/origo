eabf37970f0fe86ef9af9b85c10749cbc6d7dc49
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-01-28 13:27:23 +0100

    Update writeConfig.php

--- finalfs/www/adm/writeConfig.php
+++ finalfs/www/adm/writeConfig.php
@@ -69,11 +69,17 @@
 	$mapProjectionExtent=pgBoxToText(array_column_search($map['projectioncode'], 'code', $proj4defs)['projectionextent']);
 	if (empty($mapProjectionExtent))
 	{
-		require_once("./constants/proxyRoot.php");
-		echo '<script>alert("Projektionsutbredning saknas! Ingen konfiguration skriven."); window.location.href="'.$proxyRoot.$_SERVER["REQUEST_URI"].'&badJson=y";</script>';
-		exit;
+		if ($map['projectioncode'] != 'EPSG:3857' && $map['projectioncode'] != 'EPSG:4326')
+		{
+			require_once("./constants/proxyRoot.php");
+			echo '<script>alert("Projektionsutbredning saknas! Ingen konfiguration skriven."); window.location.href="'.$proxyRoot.$_SERVER["REQUEST_URI"].'&badJson=y";</script>';
+			exit;
+		}
 	}
-	$json = $json.'"projectionExtent": ['.$mapProjectionExtent.'], ';
+	else
+	{
+		$json = $json.'"projectionExtent": ['.$mapProjectionExtent.'], ';
+	}
 	$json = $json.'"featureinfoOptions": '.$map['featureinfooptions'].', ';
 	if (!empty($map['tilegrid']))
 	{
