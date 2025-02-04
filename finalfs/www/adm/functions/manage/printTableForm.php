9f3d64d279fd6c4491833febe6fa24a35c280a3a
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-02-04 12:24:40 +0100

    Update printTableForm.php

--- finalfs/www/adm/functions/manage/printTableForm.php
+++ finalfs/www/adm/functions/manage/printTableForm.php
@@ -6,6 +6,7 @@
 	require_once("./functions/manage/printUpdateButton.php");
 	require_once("./functions/manage/printInfoButton.php");
 	require_once("./functions/manage/printDeleteButton.php");
+	require_once("./functions/manage/targetId.php");
 
 	// Takes a full table target (array), pg_connect connection string, table selectables (array), inheritPosts (array), and helps (array).
 	// Prints form fields and buttons that are used to view and edit the configuration for the given table.
@@ -22,8 +23,9 @@
 		printUpdateSelect($table, array('contact'=>$selectables['contacts']), 'bodySelect', 'Kontakt:', in_array('contact', $helps));
 		printUpdateSelect($table, array('origin'=>$selectables['origins']), 'bodySelect', 'Ursprungskälla:', in_array('origin', $helps));
 		//printTextarea($table, 'updated', 'textareaMedium', 'Uppdaterad (åååå-mm-dd):', in_array('updated', $helps));
+		//echo '<span style="vertical-align: text-bottom"> Updaterad: <iframe src="updated.php?table='.substr(targetId($table), strpos(targetId($table), '.')+1).'" style="width:6rem; height:1rem; margin:0; padding: 0; border:none"></iframe></span>';
 		$dbh2=dbh($dbhConnectionString);
-		$tableWithSchema=substr($table['table']['table_id'], strpos($table['table']['table_id'], '.')+1);
+		$tableWithSchema=substr(targetId($table), strpos(targetId($table), '.')+1);
 		$updated=substr(updated_from_table($dbh2, $tableWithSchema)[0], 0, 10);
 		echo '<span style="vertical-align: text-bottom; margin-left:0.6em">Updaterad: </span><textarea readonly rows="1" class="textareaMedium">'.$updated.'</textarea>';
 		printUpdateSelect($table, array('update'=>$selectables['updates']), 'bodySelect', 'Uppdatering:', in_array('update', $helps));
@@ -34,7 +36,7 @@
 		printUpdateButton('table');
 		$table=makeTargetBasic($table);
 		printInfoButton($table);
-		$deleteConfirmStr="Är du säker att du vill radera all metadata för tabellen ".$table['table']."?";
+		$deleteConfirmStr="Är du säker att du vill radera all metadata för tabellen ".targetId($table)."?";
 		printDeleteButton($table, $deleteConfirmStr, $inheritPosts);
 		echo '</div></form></div></div>';
 	}
