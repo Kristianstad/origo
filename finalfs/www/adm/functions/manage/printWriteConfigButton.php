c26a36850005a5d8ce932e1eea88ab4c995eace0
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-02-04 14:25:12 +0100

    Update printWriteConfigButton.php

--- finalfs/www/adm/functions/manage/printWriteConfigButton.php
+++ finalfs/www/adm/functions/manage/printWriteConfigButton.php
@@ -5,7 +5,7 @@
 		$confirmStr="Är du säker att du vill skriva över den befintliga konfigurationen för $mapId?";
 		echo <<<HERE
 			<form onsubmit='confirmStr="{$confirmStr}"; return confirm(confirmStr);' action="writeConfig.php" method="get" target="hiddenFrame">
-				<button class="updateButton" type="submit" name="map" value="{$mapId}">
+				<button title="Skriv konfiguration (json-fil)" class="updateButton" type="submit" name="map" value="{$mapId}">
 					Skriv kartkonfiguration
 				</button>
 			</form>
