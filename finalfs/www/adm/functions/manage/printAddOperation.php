a983ceec2d28d2594709af96e26eeb4c951c7e52
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-02-05 15:11:42 +0100

    Update printAddOperation.php

--- finalfs/www/adm/functions/manage/printAddOperation.php
+++ finalfs/www/adm/functions/manage/printAddOperation.php
@@ -2,17 +2,22 @@
 
 	require_once("./functions/manage/printSelectOptions.php");
 	require_once("./functions/manage/printHiddenInputs.php");
+	require_once("./functions/manage/targetType.php");
+	require_once("./functions/toSwedish.php");
 
 	function printAddOperation($target, $addToTable, $buttontext, $inheritPosts)
 	{
-		$str=rtrim(ucfirst(key($addToTable)), 's');
+		$addToTableType=rtrim(key($addToTable), 's');
+		$str=ucfirst($addToTableType);
 		echo '<form class="addForm" method="post">';
 		echo '<select class="addSelect" name="to'.$str.'Id">';
 		printSelectOptions(array_merge(array(""),current($addToTable)));
 		echo '</select>&nbsp;';
 		printHiddenInputs($inheritPosts);
-		echo '<button type="submit" name="'.key($target).'Button" value="operation">'.$buttontext.'</button>';
+		$targetTypeSwe=toSwedish(targetType($target));
+		$addToTableTypeSwe=toSwedish($addToTableType);
+		echo '<button title="Lägg {$targetTypeSwe} till {$addToTableTypeSwe}" type="submit" name="'.key($target).'Button" value="operation">'.$buttontext.'</button>';
 		echo '</form>';
 	}
-	
+
 ?>
