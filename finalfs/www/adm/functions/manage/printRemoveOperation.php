00a600f7d478500c6e0f05a9cb0eedcc41d8f8b1
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-02-06 10:25:03 +0100

    Update printRemoveOperation.php

--- finalfs/www/adm/functions/manage/printRemoveOperation.php
+++ finalfs/www/adm/functions/manage/printRemoveOperation.php
@@ -3,20 +3,26 @@
 	require_once("./functions/findParents.php");
 	require_once("./functions/manage/printSelectOptions.php");
 	require_once("./functions/manage/printHiddenInputs.php");
+	require_once("./functions/manage/targetType.php");
+	require_once("./functions/toSwedish.php");
 
 	function printRemoveOperation($targetToRemove, $tableToRemoveFrom, $buttontext, $inheritPosts)
 	{
+		$tableToRemoveFromType=rtrim(key($tableToRemoveFrom), 's');
+		$str=ucfirst($tableToRemoveFromType);
 		$parents=findParents($tableToRemoveFrom, $targetToRemove);
 		if (!empty($parents))
 		{
 			echo '<form class="addForm" method="post">';
-			echo '<select class="addSelect" name="from'.ucfirst(rtrim(key($tableToRemoveFrom), 's')).'Id">';
+			echo '<select class="addSelect" name="from'.$str.'Id">';
 			printSelectOptions(array_merge(array(""), $parents));
 			echo '</select>&nbsp;';
 			printHiddenInputs($inheritPosts);
-			echo '<button type="submit" name="'.key($targetToRemove).'Button" value="operation">'.$buttontext.'</button>';
+			$targetToRemoveTypeSwe=toSwedish(targetType($targetToRemove));
+			$tableToRemoveFromTypeSwe=toSwedish($tableToRemoveFromType);
+			echo '<button title="Ta bort '.$targetToRemoveTypeSwe.' från '.$tableToRemoveFromTypeSwe.'" type="submit" name="'.key($targetToRemove).'Button" value="operation">'.$buttontext.'</button>';
 			echo '</form>';
 		}
 	}
-	
+
 ?>
