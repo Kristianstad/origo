24536eb989b7481550935c3c72878a8f31e007a6
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-02-10 11:03:11 +0100

    Update printUpdateSelect.php

--- finalfs/www/adm/functions/manage/printUpdateSelect.php
+++ finalfs/www/adm/functions/manage/printUpdateSelect.php
@@ -8,7 +8,7 @@
 	// Takes a full target array, parameter values (array), style class (string), label (string), help-exist (boolean), and select options (optional, array, needs to have equal number of elements as parameter two).
 	// Prints a drop-down selection box with option values to choose from. It's the parameter value of the chosen option that is set to be posted on form submit.
 	// Parameter three and four are used to set the class and label for the select tag. A help-button is printed if a help exists.
-	function printUpdateSelect($fullTarget, $configParamValues, $class, $label, $help=false, $options=null)
+	function printUpdateSelect($fullTarget, $configParamValues, $class, $label, $help=false, $options=null, $onchange='')
 	{
 		if (!isFullTarget($fullTarget))
 		{
@@ -23,7 +23,7 @@
 		echo <<<HERE
 			<span>
 				<label title="{$targetType}:{$configParam}" for='{$targetId}{$ucConfigParam}'>{$label}</label>
-				<select class='{$class}' id='{$targetId}{$ucConfigParam}' name='{$sName}'>
+				<select onchange='{$onchange}' class='{$class}' id='{$targetId}{$ucConfigParam}' name='{$sName}'>
 		HERE;
 		if (!isset($options))
 		{
