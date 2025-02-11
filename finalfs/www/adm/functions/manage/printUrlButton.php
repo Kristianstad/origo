eb01ceffd8db0bdf1881d64fe4ba1481f7cc9658
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-02-11 11:54:08 +0100

    Update printUrlButton.php

--- finalfs/www/adm/functions/manage/printUrlButton.php
+++ finalfs/www/adm/functions/manage/printUrlButton.php
@@ -2,6 +2,7 @@
 
 	function printUrlButton($url)
 	{
+		$url=str_replace('#', '%23', $url);
 		echo <<<HERE
 			<form action="$url" method="get" target="_blank">
 				<button title="Öppna karta i nytt fönster" type="submit">
