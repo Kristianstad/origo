d2bf9efa8f8811d820587fa8288d1a0cd5b35045
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-03-10 11:26:18 +0100

    Update validateUpdate.php

--- finalfs/www/adm/functions/manage/validateUpdate.php
+++ finalfs/www/adm/functions/manage/validateUpdate.php
@@ -8,13 +8,22 @@
 	function validateUpdate($updatePosts, $configTables, &$updateValid)
 	{
 		require("./constants/multiselectables.php");
+		require("./constants/tableAliases.php");
 		$updateValid=true;
 		foreach ($multiselectables as $table)
 		{
 			if (!empty($updatePosts['update'.ucfirst($table)]))
 			{
 				$updateValues=explode(',', $updatePosts['update'.ucfirst($table)]);
-				$unknownValues=array_diff($updateValues, array_column($configTables[$table], pkColumnOfTable($table)));
+				if (!empty($tableAliases[$table]))
+				{
+					$trueTable=$tableAliases[$table];
+				}
+				else
+				{
+					$trueTable=$table;
+				}
+				$unknownValues=array_diff($updateValues, array_column($configTables[$trueTable], pkColumnOfTable($trueTable)));
 				if (!empty($unknownValues))
 				{
 					echo '<script>window.onload=function(){alert("Uppdatering misslyckades!\nOkända värden för '.toSwedish($table).': '.implode(', ', $unknownValues).'");}</script>';
