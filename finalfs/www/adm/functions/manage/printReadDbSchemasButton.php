4e2f7245b4abc7861e1fde2acf7ee9caca67cc58
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-03-21 10:14:32 +0100

    Update printReadDbSchemasButton.php

--- finalfs/www/adm/functions/manage/printReadDbSchemasButton.php
+++ finalfs/www/adm/functions/manage/printReadDbSchemasButton.php
@@ -5,7 +5,7 @@
 		$confirmStr="Läser in eventuella nya scheman från databasen (utan tabeller). Befintiga metadata påverkas ej.";
 		echo <<<HERE
 			<form id='readDbSchemas' onsubmit='confirmStr="{$confirmStr}"; confirm(confirmStr); setTimeout(function() {document.getElementById("databasesHeadForm").submit();}, 1000);' action="read_db_schemas.php" method="get" target="hiddenFrame">
-				<button class="updateButton" type="submit" name="database" value="{$databaseId}">
+				<button title="Uppdatera verktyget med nya scheman från databasen" class="updateButton" type="submit" name="database" value="{$databaseId}">
 					Läs in nya scheman
 				</button>
 			</form>
