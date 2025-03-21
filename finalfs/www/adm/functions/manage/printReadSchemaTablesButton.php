1a4bc9adbe1f95f01e49e515c9d60d2e0e43a508
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-03-21 10:15:36 +0100

    Update printReadSchemaTablesButton.php

--- finalfs/www/adm/functions/manage/printReadSchemaTablesButton.php
+++ finalfs/www/adm/functions/manage/printReadSchemaTablesButton.php
@@ -5,7 +5,7 @@
 		$confirmStr="Läser in eventuella nya tabeller från schemat. Befintiga metadata påverkas ej.";
 		echo <<<HERE
 			<form onsubmit='confirmStr="{$confirmStr}"; confirm(confirmStr); setTimeout(function() {if (document.getElementById("schemas1HeadForm") != null) {document.getElementById("schemas1HeadForm").submit();} else {document.getElementById("schemasHeadForm").submit();}}, 1000);' action="read_schema_tables.php" method="get" target="hiddenFrame">
-				<button class="updateButton" type="submit" name="schema" value="{$schemaId}">
+				<button title="Uppdatera verktyget med nya tabeller från schemat" class="updateButton" type="submit" name="schema" value="{$schemaId}">
 					Läs in nya tabeller
 				</button>
 			</form>
