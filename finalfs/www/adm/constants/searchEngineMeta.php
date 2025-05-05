8496964b7baa5f9d5f70a6c2c7dadda389366748
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-05-05 12:04:51 +0200

    Create searchEngineMeta.php

---
+++ finalfs/www/adm/constants/searchEngineMeta.php
@@ -0,0 +1,12 @@
+<?php
+
+	// Constants containing metadata for search engine indexing.
+	$geoLatitude = '56.0313';
+	$geoLongitude = '14.1524';
+	$contentLocationName = 'Kristianstad';
+	$contentLocationAddressLocality = 'Kristianstad';
+	$contentLocationAddressCountry = 'SE';
+	$publisherName = 'Kristianstads kommun';
+	$publisherUrl = 'https://www.kristianstad.se/';
+
+?>
