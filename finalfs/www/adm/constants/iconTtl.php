9fc6899efe160e3c6f4467cd6b268ccb44c22856
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-05-16 11:04:37 +0200

    Create iconTtl.php

---
+++ finalfs/www/adm/constants/iconTtl.php
@@ -0,0 +1,6 @@
+<?php
+
+	// The size of the ttl-parameter added to the urls of layertree icons. Set to '0' if no ttl-parameter should be added. The ttl-parameter is never added to icon-urls of restricted layers.
+	$iconTtl = '36000';
+
+?>
