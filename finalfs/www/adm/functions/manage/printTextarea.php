b5b5bd4ed8b9a49336e2fb086d0d97c38195e1f6
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-05-19 14:17:52 +0200

    Update printTextarea.php

--- finalfs/www/adm/functions/manage/printTextarea.php
+++ finalfs/www/adm/functions/manage/printTextarea.php
@@ -11,7 +11,7 @@
 	// Prints a textarea containing the configuration parameter for the given target. Class name and label for the textarea are taken from the parameter three and four. 
 	// The textarea is set to readonly if parameter six is set to true. A help button is printed if a help target exists, and a multiselect button is printed if the config
 	// parameter name exists in the multiselectables.php constant.
-	function printTextarea($fullTarget, $configParam, $class, $label, $help=false, $readonly=false)
+	function printTextarea($fullTarget, $configParam, $class, $label, $help=false, $inheritPosts=array(), $readonly=false)
 	{
 		if (!isFullTarget($fullTarget))
 		{
@@ -35,10 +35,20 @@
 		}
 		$targetId=targetId($fullTarget);
 		$targetType=targetType($fullTarget);
+		if (isset($inheritPosts['width'.$ucConfigParam], $inheritPosts['height'.$ucConfigParam]))
+		{
+			$styleStr='style=width:'.$inheritPosts['width'.$ucConfigParam].'px;height:'.$inheritPosts['height'.$ucConfigParam].'px';
+		}
+		else
+		{
+			$styleStr='';
+		}
 		echo <<<HERE
 			<span class="optionSpan">
 				<label title="{$targetType}:{$configParam}" for="{$targetId}{$ucConfigParam}">{$label}</label>
-				<textarea {$ro}rows="1" class="{$class}" id="{$targetId}{$ucConfigParam}" name="update{$ucConfigParam}">{$configParamValue}</textarea>
+				<textarea {$ro}rows="1" class="{$class}" id="{$targetId}{$ucConfigParam}" name="update{$ucConfigParam}" onmouseup="document.getElementById('{$targetId}{$ucConfigParam}_width').value = this.offsetWidth; document.getElementById('{$targetId}{$ucConfigParam}_height').value = this.offsetHeight;" onmouseover="document.getElementById('{$targetId}{$ucConfigParam}_width').value = this.offsetWidth; document.getElementById('{$targetId}{$ucConfigParam}_height').value = this.offsetHeight;" {$styleStr}>{$configParamValue}</textarea>
+				<input type="hidden" name="newwidth{$ucConfigParam}" id="{$targetId}{$ucConfigParam}_width">
+				<input type="hidden" name="newheight{$ucConfigParam}" id="{$targetId}{$ucConfigParam}_height">
 		HERE;
 		if (in_array($configParam, $multiselectables))
 		{
