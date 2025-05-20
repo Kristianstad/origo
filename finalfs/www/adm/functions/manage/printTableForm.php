645dba63fc3d0af386438e7dd83fc8c151f77fb2
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-05-20 12:30:54 +0200

    Update printTableForm.php

--- finalfs/www/adm/functions/manage/printTableForm.php
+++ finalfs/www/adm/functions/manage/printTableForm.php
@@ -1,6 +1,7 @@
 <?php
 
 	require_once("./functions/manage/isFullTarget.php");
+	require_once("./functions/manage/sizePosts.php");
 	require_once("./functions/manage/printTextarea.php");
 	require_once("./functions/manage/printHiddenInputs.php");
 	require_once("./functions/manage/printUpdateButton.php");
@@ -17,22 +18,21 @@
 		{
 			die("printTableForm($table, $selectables, $inheritPosts, $helps=array()) failed!");
 		}
+		$sizePosts=sizePosts($inheritPosts);
 		echo '<div><div class="printXFormDiv"><form method="post">';
-		printTextarea($table, 'table_id', 'textareaMedium', 'Id:', in_array('table_id', $helps));
-		printTextarea($table, 'abstract', 'textareaLarge', 'Beskrivning:', in_array('abstract', $helps));
-		printTextarea($table, 'keywords', 'textareaLarge', 'Nyckelord:', in_array('keywords', $helps));
+		printTextarea($table, 'table_id', 'textareaMedium', 'Id:', in_array('table_id', $helps), $sizePosts);
+		printTextarea($table, 'abstract', 'textareaLarge', 'Beskrivning:', in_array('abstract', $helps), $sizePosts);
+		printTextarea($table, 'keywords', 'textareaLarge', 'Nyckelord:', in_array('keywords', $helps), $sizePosts);
 		printUpdateSelect($table, array('contact'=>$selectables['contacts']), 'bodySelect', 'Kontakt:', in_array('contact', $helps));
 		printUpdateSelect($table, array('origin'=>$selectables['origins']), 'bodySelect', 'Ursprungskälla:', in_array('origin', $helps));
-		//printTextarea($table, 'updated', 'textareaMedium', 'Uppdaterad (åååå-mm-dd):', in_array('updated', $helps));
-		//echo '<span style="vertical-align: text-bottom"> Updaterad: <iframe src="updated.php?table='.substr(targetId($table), strpos(targetId($table), '.')+1).'" style="width:6rem; height:1rem; margin:0; padding: 0; border:none"></iframe></span>';
 		$dbh2=dbh($dbhConnectionString);
 		$tableWithSchema=substr(targetId($table), strpos(targetId($table), '.')+1);
 		$updated=substr(updated_from_table($dbh2, $tableWithSchema)[0], 0, 10);
 		setTargetConfigParam($table, 'updated', $updated);
-		printTextarea($table, 'updated', 'textareaMedium', 'Uppdaterad:', in_array('updated', $helps), true);
+		printTextarea($table, 'updated', 'textareaMedium', 'Uppdaterad:', in_array('updated', $helps), $sizePosts, true);
 		printUpdateSelect($table, array('update'=>$selectables['updates']), 'bodySelect', 'Uppdatering:', in_array('update', $helps));
-		printTextarea($table, 'history', 'textareaLarge', 'Tillkomsthistorik:', in_array('history', $helps));
-		printTextarea($table, 'info', 'textareaLarge', 'Info:', in_array('info', $helps));
+		printTextarea($table, 'history', 'textareaLarge', 'Tillkomsthistorik:', in_array('history', $helps), $sizePosts);
+		printTextarea($table, 'info', 'textareaLarge', 'Info:', in_array('info', $helps), $sizePosts);
 		printHiddenInputs($inheritPosts);
 		echo '<div class="buttonDiv">';
 		printUpdateButton('table');
