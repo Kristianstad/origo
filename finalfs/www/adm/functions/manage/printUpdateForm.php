307ff2d8c2a140406b4561b15c6994ee9eab71bb
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-05-20 12:29:22 +0200

    Update printUpdateForm.php

--- finalfs/www/adm/functions/manage/printUpdateForm.php
+++ finalfs/www/adm/functions/manage/printUpdateForm.php
@@ -1,6 +1,7 @@
 <?php
 
 	require_once("./functions/manage/isFullTarget.php");
+	require_once("./functions/manage/sizePosts.php");
 	require_once("./functions/manage/printTextarea.php");
 	require_once("./functions/manage/printHiddenInputs.php");
 	require_once("./functions/manage/printUpdateButton.php");
@@ -16,13 +17,14 @@
 		{
 			die("printUpdateForm($update, $inheritPosts, $helps=array()) failed!");
 		}
+		$sizePosts=sizePosts($inheritPosts);
 		echo '<div><div class="printXFormDiv"><form method="post">';
-		printTextarea($update, 'update_id', 'textareaMedium', 'Id:', in_array('update_id', $helps));
-		printTextarea($update, 'name', 'textareaMedium', 'Name:', in_array('name', $helps));
+		printTextarea($update, 'update_id', 'textareaMedium', 'Id:', in_array('update_id', $helps), $sizePosts);
+		printTextarea($update, 'name', 'textareaMedium', 'Name:', in_array('name', $helps), $sizePosts);
 		printUpdateSelect($update, array('interval'=>array("+1 day", "+1 week", "+1 month", "+1 year")), 'miniSelect', 'Intervall:', in_array('interval', $helps));
 		printUpdateSelect($update, array('method'=>array("manuellt", "automatiskt")), 'miniSelect', 'Metod:', in_array('method', $helps));
-		printTextarea($update, 'abstract', 'textareaLarge', 'Beskrivning:', in_array('abstract', $helps));
-		printTextarea($update, 'info', 'textareaLarge', 'Info:', in_array('info', $helps));
+		printTextarea($update, 'abstract', 'textareaLarge', 'Beskrivning:', in_array('abstract', $helps), $sizePosts);
+		printTextarea($update, 'info', 'textareaLarge', 'Info:', in_array('info', $helps), $sizePosts);
 		printHiddenInputs($inheritPosts);
 		echo '<div class="buttonDiv">';
 		printUpdateButton('update');
