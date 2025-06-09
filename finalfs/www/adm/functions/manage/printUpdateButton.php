4ffc1440db8218c1d3dd8c031d55e5abfdb3e25a
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-06-09 15:03:51 +0200

    Update printUpdateButton.php

--- finalfs/www/adm/functions/manage/printUpdateButton.php
+++ finalfs/www/adm/functions/manage/printUpdateButton.php
@@ -3,7 +3,17 @@
 	// Takes a type (string) and prints a form submit-button with class="updateButton", name="<type>Button", value="update", and the label "Uppdatera".
 	function printUpdateButton($type)
 	{
-		echo '<button title="Skriv ändringar till databas" class="updateButton" type="submit" name="'.$type.'Button" value="update">Uppdatera</button>';
+		GLOBAL $formChangedGlobal;
+		if ($formChangedGlobal)
+		{
+			$formChangedGlobal=false;
+			$changeClass=' change';
+		}
+		else
+		{
+			$changeClass='';
+		}
+		echo '<button title="Skriv ändringar till databas" class="updateButton'.$changeClass.'" type="submit" name="'.$type.'Button" value="update">Uppdatera</button>';
 	}
 
 ?>
