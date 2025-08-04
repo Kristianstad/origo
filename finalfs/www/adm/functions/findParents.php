8af56c507e30e82b2a893fca01610d36bd554396
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-08-04 15:02:08 +0200

    Update findParents.php

--- finalfs/www/adm/functions/findParents.php
+++ finalfs/www/adm/functions/findParents.php
@@ -1,24 +1,35 @@
 <?php
 
+	require_once("./functions/manage/isTarget.php");
+	require_once("./functions/manage/makeTargetBasic.php");
 	require_once("./functions/pgArrayToPhp.php");
 	require_once("./functions/pkColumnOfTable.php");
 
-	function findParents($potentialParents, $child)
+	function findParents($potentialParents, $target)
 	{
+		if (isTarget($target))
+		{
+			$target=makeTargetBasic($target);
+		}
+		else
+		{
+			die("findParents(\$potentialParents, $target) failed! Child not a target.");
+		}
+		
 		require("./constants/arrayColumns.php");
 		$parents=array();
 		foreach (current($potentialParents) as $potentialParent)
 		{
-			if (in_array(key($child).'s', $arrayColumns))
+			if (in_array(key($target).'s', $arrayColumns))
 			{
-				if (in_array(current($child), pgArrayToPhp($potentialParent[key($child).'s'])))
+				if (in_array(current($target), pgArrayToPhp($potentialParent[key($target).'s'])))
 				{
 					$parents[]=$potentialParent[pkColumnOfTable(key($potentialParents))];
 				}
 			}
 			else
 			{
-				if (current($child) == $potentialParent[key($child)])
+				if (current($target) == $potentialParent[key($target)])
 				{
 					$parents[]=$potentialParent[pkColumnOfTable(key($potentialParents))];
 				}
@@ -26,5 +37,5 @@
 		}
 		return $parents;
 	}
-	
+
 ?>
