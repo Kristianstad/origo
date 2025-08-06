02a4bb912b19b5a9330bb34957fb5f9bdd731892
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-08-06 11:44:58 +0200

    Update printDeleteButton.php

--- finalfs/www/adm/functions/manage/printDeleteButton.php
+++ finalfs/www/adm/functions/manage/printDeleteButton.php
@@ -9,46 +9,50 @@
 	// If OK is pressed in the confirmation popup then the target is posted to manage.php for deletion.
 	function printDeleteButton($target, $deleteConfirmStr, $inheritPosts)
 	{
-		$targetType=key($target);
-		$targetId=current($target);
-		/*
-		if ($targetType == 'map')
+		GLOBAL $viewDepthGlobal;
+		if ($viewDepthGlobal == 1)
 		{
-			$inheritPosts=array();
-		}
-		elseif ($targetType == 'group')
-		{
-			$groupIdsArr=explode(',', $inheritPosts['groupIds']);
-			foreach (array_reverse($groupIdsArr, true) as $k => $v)
+			$targetType=key($target);
+			$targetId=current($target);
+			/*
+			if ($targetType == 'map')
 			{
-				unset($groupIdsArr[$k]);
-				if ($v == $targetId)
+				$inheritPosts=array();
+			}
+			elseif ($targetType == 'group')
+			{
+				$groupIdsArr=explode(',', $inheritPosts['groupIds']);
+				foreach (array_reverse($groupIdsArr, true) as $k => $v)
 				{
-					break;
+					unset($groupIdsArr[$k]);
+					if ($v == $targetId)
+					{
+						break;
+					}
+				}
+				$inheritPosts['groupIds']=implode(',', $groupIdsArr);
+			}
+			else
+			{
+				foreach ($inheritPosts as $k => $v)
+				{
+					unset($inheritPosts[$k]);
+					if ($k == $targetType.'Id' && $v == $targetId)
+					{
+						break;
+					}
 				}
 			}
-			$inheritPosts['groupIds']=implode(',', $groupIdsArr);
+			*/
+			$targetTypeSwe=toSwedish($targetType);
+			echo <<<HERE
+					<form method='post' onsubmit='confirmStr="{$deleteConfirmStr}"; return confirm(confirmStr);'>
+						<input type="hidden" name="{$targetType}IdDel" value="{$targetId}">
+						<button title='Radera {$targetTypeSwe}' class='deleteButton' type='submit' name='{$targetType}Button' value='delete'>Radera</button>
+			HERE;
+			printHiddenInputs($inheritPosts);
+			echo '</form>';
 		}
-		else
-		{
-			foreach ($inheritPosts as $k => $v)
-			{
-				unset($inheritPosts[$k]);
-				if ($k == $targetType.'Id' && $v == $targetId)
-				{
-					break;
-				}
-			}
-		}
-		*/
-		$targetTypeSwe=toSwedish($targetType);
-		echo <<<HERE
-				<form method='post' onsubmit='confirmStr="{$deleteConfirmStr}"; return confirm(confirmStr);'>
-					<input type="hidden" name="{$targetType}IdDel" value="{$targetId}">
-					<button title='Radera {$targetTypeSwe}' class='deleteButton' type='submit' name='{$targetType}Button' value='delete'>Radera</button>
-		HERE;
-		printHiddenInputs($inheritPosts);
-		echo '</form>';
 	}
 
 ?>
