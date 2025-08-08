e5b94a80543755f8b560364556f348803ffd234b
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-08-08 09:48:46 +0200

    Update printWriteConfigButton.php

--- finalfs/www/adm/functions/manage/printWriteConfigButton.php
+++ finalfs/www/adm/functions/manage/printWriteConfigButton.php
@@ -1,11 +1,20 @@
 <?php
 
-	function printWriteConfigButton($mapId)
+	// Takes a map-id and (optionally) a $changed-value, and prints a "Write configuration to disk (json)"-button.
+	function printWriteConfigButton($mapId, $changed='f')
 	{
+		if ($changed == 't')
+		{
+			$changeClass=' change';
+		}
+		else
+		{
+			$changeClass='';
+		}
 		$confirmStr="Är du säker att du vill skriva över den befintliga konfigurationen för $mapId?";
 		echo <<<HERE
-			<form onsubmit='confirmStr="{$confirmStr}"; return confirm(confirmStr);' action="writeConfig.php" method="get" target="hiddenFrame">
-				<button title="Skriv konfiguration till disk (json)" class="updateButton" type="submit" name="map" value="{$mapId}">
+			<form onsubmit='confirmStr="{$confirmStr}"; if (confirm(confirmStr)) {this.children[0].classList.remove("change"); return true;} else {return false;}' action="writeConfig.php" method="get" target="hiddenFrame">
+				<button title="Skriv konfiguration till disk (json)" class="updateButton{$changeClass}" type="submit" name="map" value="{$mapId}">
 					Skriv kartkonfiguration
 				</button>
 			</form>
