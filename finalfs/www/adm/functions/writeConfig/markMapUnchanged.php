e3ce18d29e9bc61929059a03129264d92d9fe745
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-08-08 09:56:01 +0200

    Create markMapUnchanged.php

---
+++ finalfs/www/adm/functions/writeConfig/markMapUnchanged.php
@@ -0,0 +1,15 @@
+<?php
+
+	// Takes a database handle and an map-id. Sets column "changed" to "f" in the database for the given map.
+	function markMapUnchanged(&$dbh, $mapId)
+	{
+		require("./constants/configSchema.php");
+		$sql=$sql."UPDATE $configSchema.maps SET changed = 'f' WHERE map_id = '$mapId'; ";
+		$result=pg_query($dbh, $sql);
+		if (!$result)
+		{
+			die("Error in SQL query: " . pg_last_error());
+		}
+	}
+
+?>
