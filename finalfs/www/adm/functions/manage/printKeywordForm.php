2a98f28406f43daf7876d9b0a73e5ad87a9bedfc
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-08-11 09:59:51 +0200

    Update printKeywordForm.php

--- finalfs/www/adm/functions/manage/printKeywordForm.php
+++ finalfs/www/adm/functions/manage/printKeywordForm.php
@@ -1,13 +1,6 @@
 <?php
 
-	require_once("./functions/manage/isFullTarget.php");
-	require_once("./functions/manage/sizePosts.php");
-	require_once("./functions/manage/printTextarea.php");
-	require_once("./functions/manage/printHiddenInputs.php");
-	require_once("./functions/manage/printUpdateButton.php");
-	require_once("./functions/manage/printInfoButton.php");
-	require_once("./functions/manage/printDeleteButton.php");
-	require_once("./functions/manage/targetId.php");
+	// Uses manage functions: isFullTarget, sizePosts, printTextarea, printHiddenInputs, printUpdateButton, printInfoButton, printDeleteButton, targetId
 
 	// Takes a full keyword target (array), inheritPosts (array), and helps (array).
 	// Prints form fields and buttons that are used to view and edit the configuration for the given keyword.
