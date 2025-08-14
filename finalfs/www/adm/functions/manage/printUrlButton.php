6ca987efa41a7690b249b8facc259c0c056fed37
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-08-14 12:46:57 +0200

    Update printUrlButton.php

--- finalfs/www/adm/functions/manage/printUrlButton.php
+++ finalfs/www/adm/functions/manage/printUrlButton.php
@@ -3,8 +3,8 @@
 	function printUrlButton($url)
 	{
 		echo <<<HERE
-			<form action="$url" method="get" target="_blank">
-				<button title="Öppna karta i nytt fönster" type="submit">
+			<form>
+				<button title="Öppna karta i nytt fönster" type="button" onclick="window.open('$url', '_blank')">
 					Öppna karta
 				</button>
 			</form>
