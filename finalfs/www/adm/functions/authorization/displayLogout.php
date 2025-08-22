e15c72abf9387d29c7cf524612f8b9ba4b6d1027
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-08-22 12:45:38 +0200

    Create displayLogout.php

---
+++ finalfs/www/adm/functions/authorization/displayLogout.php
@@ -0,0 +1,18 @@
+<?php
+
+	function displayLogout()
+	{
+		$user = $_SESSION["user"]['id'];
+		echo <<<HERE
+					<script>
+						sessionStorage.user_id="{$user}";
+					</script>
+					<b style="color:#000000">{$_SESSION['user']['id']} är inloggad! </b>
+					<button id="loginbtn" style="cursor:pointer;background:#eee;border-radius:1rem;border:#eee;width:auto;text-align:center;white-space:nowrap;padding: 0.5rem 0.75rem;font:14px Segoe UI,Roboto,Helvetica Neue,Arial,sans-serif;" type="button" onclick="sessionStorage.removeItem('user_id'); document.location.assign('./authorization.php?logout');">Logga ut</button>
+					</br><iframe src="./news.php?action=subjects" style="border:none;width:100%;margin-top:5px;margin-bottom:10px"></iframe>
+		HERE;
+		fastcgi_finish_request();
+		exit(0);
+	}
+
+?>
