b26e596a736a26a30830e5d134d77a611be1c3e4
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-08-22 12:38:25 +0200

    Create defineFileConstant.php

---
+++ finalfs/www/adm/functions/common/defineFileConstant.php
@@ -0,0 +1,9 @@
+<?php
+
+	function defineFileConstant($key, $value)
+	{
+		file_put_contents("./tmp/$key.php", "<?php\ndefine('$key', ".var_export($value, true).');');
+		define($key, $value);
+	}
+
+?>
