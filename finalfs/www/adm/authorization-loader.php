4710d29a4111955d9fb8bebfeeadef2202c9a0ea
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-08-22 12:29:28 +0200

    Create authorization-loader.php

---
+++ finalfs/www/adm/authorization-loader.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html style="width:100%;height:100%">
+<head>
+</head>
+<body>
+<iframe src="./authorization.php" style="width:100%;height:100%;margin-top:-15px"></iframe>
+</body>
+</html>
