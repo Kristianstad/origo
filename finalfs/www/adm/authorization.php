4db3abb16e36d4b9faa7b26d4a509f29a722d103
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-08-22 12:27:05 +0200

    Create authorization.php

---
+++ finalfs/www/adm/authorization.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<style>
+		<?php require("./styles/authorization.css"); ?>
+	</style>
+</head>
+<body>
+<?php
+	// Tell browsers to not cache response
+	header("Cache-Control: must-revalidate, max-age=0, s-maxage=0, no-cache, no-store");
+	
+	// Expose specific functions
+	require_once("./functions/includeDirectory.php");
+	//require_once("./functions/adldap/autoload.php");
+	
+	// Expose all functions in given folders
+	includeDirectory("./functions/common");
+	includeDirectory("./functions/authorization");
+	
+	session_start(array('read_and_close' => true));
+	includeFileConstant('RESTRICTEDLAYERS');
+	initUser();
+
+	//ini_set('output_buffering', 'off');
+
+	if (isset($_GET['logout']))
+	{
+		logout();
+		exit(0);
+	}
+	elseif (isset($_GET['displaylogout']) || 
+		!isset($_GET['SERVICE']) && 
+		!empty($_SESSION['user']))
+	{
+		displayLogout();
+		exit(0);
+	}
+	elseif ( $_SERVER["REQUEST_METHOD"] == "POST" )
+	{
+		login();
+		exit(0);
+	}
+	else
+	{
+		displayLogin();
+		exit(0);
+	}
+?>
+</body>
+</html>
