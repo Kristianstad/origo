1aca584c22f67961512cd34d724a40837cf2b87a
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-08-25 14:51:16 +0200

    Create authorization-iframe.php

---
+++ finalfs/www/authorization/authorization-iframe.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html style="width:100%;height:100%">
+	<body>
+		<?php $src=dirname($_SERVER["PHP_SELF"]).'/authorization-loader.php'; ?>
+		<iframe src="<?php echo $src; ?>" style="width:100%;height:100%;margin-top:-15px"></iframe>
+	</body>
+</html>
