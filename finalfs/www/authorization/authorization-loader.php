cea07f3a483dd10c453f4d85ce3fe1ce26357180
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-08-25 14:52:43 +0200

    Create authorization-loader.php

---
+++ finalfs/www/authorization/authorization-loader.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html style="width:100%;height:100%">
+	<head>
+		<base href='/php/adm/'>
+	</head>
+	<body>
+		<?php
+			chdir('../adm/');
+			require ('../adm/authorization.php');
+		?>
+	</body>
+</html>
