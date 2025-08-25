940b16de2e51d0ded643fff7d794576d49a11264
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-08-25 15:21:26 +0200

    Update and rename authorization-loader.php to authorization-iframe.php

--- finalfs/www/adm/authorization-loader.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html style="width:100%;height:100%">
-<head>
-</head>
-<body>
-<iframe src="./authorization.php" style="width:100%;height:100%;margin-top:-15px"></iframe>
-</body>
-</html>
