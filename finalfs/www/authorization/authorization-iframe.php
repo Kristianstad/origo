ee86e16f8062bf040d7c0d8675d5cc2f19caca6d
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-08-26 12:25:04 +0200

    Update authorization-iframe.php

--- finalfs/www/authorization/authorization-iframe.php
+++ finalfs/www/authorization/authorization-iframe.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html style="width:100%;height:100%">
 	<body>
-		<?php $src=dirname($_SERVER["PHP_SELF"]).'/authorization-loader.php'; ?>
+		<?php 
+			require('../adm/constants/proxyRoot.php');
+			$src=$proxyRoot.dirname($_SERVER["PHP_SELF"]).'/authorization-loader.php';
+		?>
 		<iframe src="<?php echo $src; ?>" style="width:100%;height:100%;margin-top:-15px"></iframe>
 	</body>
 </html>
