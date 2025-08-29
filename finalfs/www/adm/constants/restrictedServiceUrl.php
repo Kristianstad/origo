0e938c8dfba59d3d27e254bf59b7d25e5457bee1
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-08-29 12:22:05 +0200

    Create restrictedServiceUrl.php

---
+++ finalfs/www/adm/constants/restrictedServiceUrl.php
@@ -0,0 +1,6 @@
+<?php
+
+	// A web address to restricted map services. The address need to be accessible from the origo server but not public.
+	$restrictedServiceUrl = 'http://qgisserver:8080/';
+
+?>
