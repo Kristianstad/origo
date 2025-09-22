caa114d604bff83b374187f19a67cd1d803efcfc
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-09-22 12:03:49 +0200

    Update copyTextById.js

--- finalfs/www/adm/js-functions/multiselect/copyTextById.js
+++ finalfs/www/adm/js-functions/multiselect/copyTextById.js
@@ -7,6 +7,7 @@ function copyTextById(id)
 	copyText.select();
 	copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
-	/* Copy the text inside the text field */
+	/* Copy the text inside the text field
+	NOTE! Does only work for https */
 	navigator.clipboard.writeText(copyText.value);
 }
