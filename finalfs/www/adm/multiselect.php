779397c7240b19026bf02f2b61b698675909a053
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-09-23 16:43:32 +0200

    Update multiselect.php

--- finalfs/www/adm/multiselect.php
+++ finalfs/www/adm/multiselect.php
@@ -13,17 +13,6 @@
 <head>
 	<script>
 		<?php includeDirectory("./js-functions/multiselect"); ?>
-        // Initialize event listeners
-        $(document).ready(function() {
-            document.addEventListener('mousedown', handleMouseDown);
-            $('#clear-button').on('click', function() {
-                $('#selection').val('').text('');
-                $('select[multiple]').val([]).trigger('change');
-            });
-            $('#reset-button').on('click', function() {
-                window.location.reload();
-            });
-        });
 	</script>
 	<style>
 		<?php require("./styles/multiselect.css"); ?>
