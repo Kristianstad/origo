596f436d7c2005a0791536431e953f1ad5a3000a
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-09-23 16:45:42 +0200

    Update update.js

--- finalfs/www/adm/js-functions/multiselect/update.js
+++ finalfs/www/adm/js-functions/multiselect/update.js
@@ -1,57 +1,53 @@
 function update(menu) {
     let selection = document.querySelector("#selection");
-    let sortedValues = $(menu).attr('data-sorted-values') ? $(menu).attr('data-sorted-values').split(',') : [];
-    let currentValues = $(menu).val() || []; // Get selected values or empty array
+    let sortedValues = menu.dataset.sortedValues ? menu.dataset.sortedValues.split(',') : [];
+    let currentValues = Array.from(menu.selectedOptions).map(option => option.value) || [];
     let last_action = '';
 
-    // console.log('update called', 'menu:', $(menu).prop('id'), 'val:', currentValues, 'initial sorted-values:', sortedValues);
+    /*console.log('update called', 'menu:', menu.id, 'val:', currentValues, 'initial sorted-values:', sortedValues);*/
 
-    // Nothing selected
+    /* Nothing selected */
     if (currentValues.length === 0) {
-        // console.log('Nothing selected case');
-        sortedValues = []; // Clear array
+        /*console.log('Nothing selected case');*/
+        sortedValues = []; /* Clear array */
         last_action = 'nothing selected';
-        $(menu).find('option').removeAttr('selected'); // Clear all selected attributes
+        Array.from(menu.options).forEach(option => {
+            option.selected = false; /* Clear all selected attributes */
+        });
     } else {
-        // Get the text of currently selected options
-        let selectedTexts = $(menu)
-            .find('option')
-            .filter(':selected')
-            .map(function () {
-                return $(this).text();
-            })
-            .get();
+        /* Get the text of currently selected options */
+        let selectedTexts = Array.from(menu.selectedOptions).map(option => option.text);
 
-        // console.log('Selected values:', currentValues, 'Selected texts:', selectedTexts);
+        /*console.log('Selected values:', currentValues, 'Selected texts:', selectedTexts);*/
 
-        // Determine added and removed items
+        /* Determine added and removed items */
         let added = selectedTexts.filter(text => !sortedValues.includes(text));
         let removed = sortedValues.filter(text => !selectedTexts.includes(text));
 
-        // Update last_action
+        /* Update last_action */
         if (added.length > 0) {
             last_action = 'added: ' + added.join(', ');
         } else if (removed.length > 0) {
             last_action = 'removed: ' + removed.join(', ');
         }
 
-        // Remove deselected items from sortedValues
+        /* Remove deselected items from sortedValues */
         sortedValues = sortedValues.filter(text => selectedTexts.includes(text));
 
-        // Append new selections to the end of sortedValues
+        /* Append new selections to the end of sortedValues */
         sortedValues = sortedValues.concat(added);
 
-        // Ensure the selected attributes are in sync
-        $(menu).find('option').each(function () {
-            $(this).prop('selected', currentValues.includes($(this).val()));
+        /* Ensure the selected attributes are in sync */
+        Array.from(menu.options).forEach(option => {
+            option.selected = currentValues.includes(option.value);
         });
     }
 
-    // Update the data attribute and UI
+    /* Update the data attribute and UI */
     let sortedValuesString = sortedValues.join(',');
-    $(menu).attr('data-sorted-values', sortedValuesString);
-    $('#selection').html(sortedValuesString);
+    menu.dataset.sortedValues = sortedValuesString;
+    selection.innerHTML = sortedValuesString;
     selection.value = sortedValuesString;
 
-    // console.log('Final sorted-values:', sortedValuesString, 'Selection:', selection.value, 'last_action:', last_action);
+    /*console.log('Final sorted-values:', sortedValuesString, 'Selection:', selection.value, 'last_action:', last_action);*/
 }
