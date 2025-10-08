765f5e635b4596f27530f16173c01b37cb69efed
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-10-08 10:27:27 +0200

    Update arrayColumns.php

--- finalfs/www/adm/constants/arrayColumns.php
+++ finalfs/www/adm/constants/arrayColumns.php
@@ -1,6 +1,6 @@
 <?php
 
 	// An array of column names, which exists (in one ore more places) in the Origo configuration database tables, that allways contains array data
-	$arrayColumns=array('controls', 'groups', 'layers', 'proj4defs', 'exports', 'categories', 'adusers', 'adgroups', 'tables', 'resolutions', 'keywords', 'css_files', 'js_files', 'formats', 'reads', 'deletes');
+	$arrayColumns=array('controls', 'groups', 'layers', 'proj4defs', 'exports', 'categories', 'adusers', 'adgroups', 'tables', 'resolutions', 'keywords', 'css_files', 'js_files', 'formats', 'reads', 'deletes', 'plugins');
 
 ?>
