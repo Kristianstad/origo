6d21bffc695b73e0e62af8c9fde9b0d638ae75ac
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-12-04 13:08:15 +0100

    Update cleanupOldMapStates.php

--- finalfs/www/adm/functions/mapstate/cleanupOldMapStates.php
+++ finalfs/www/adm/functions/mapstate/cleanupOldMapStates.php
@@ -5,12 +5,15 @@ function cleanupOldMapStates($dbh, int $days): void
 {
     $cutoff = "NOW() - INTERVAL '$days days'";
     $table  = getMapStatesTable();
-
     $sql = "
         DELETE FROM $table
-        WHERE lastuse < $cutoff
-          AND NOT preserve
+        WHERE 
+            (lastuse IS NOT NULL AND lastuse < $cutoff AND NOT preserve)
+            OR
+            (created < NOW() - INTERVAL '30 days' AND lastuse IS NULL AND NOT preserve)
     ";
-
-    pg_query($dbh, $sql);
+    $result = pg_query($dbh, $sql);
+    if ($result === false) {
+        error_log("Cleanup av gamla map states misslyckades: " . pg_last_error($dbh));
+    }
 }
