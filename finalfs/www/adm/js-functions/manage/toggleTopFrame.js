b7d610847e74a4bce12f33eb1f84ee35c3ed72f4
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-12-17 10:07:59 +0100

    Update toggleTopFrame.js

--- finalfs/www/adm/js-functions/manage/toggleTopFrame.js
+++ finalfs/www/adm/js-functions/manage/toggleTopFrame.js
@@ -1,18 +1,23 @@
 function toggleTopFrame(type)
 {
-	var x = document.getElementById("topFrame");
-	if (x.style.display === "none")
-	{
-		x.style.display = "block";
-		window.scrollTo(0, 0);
-	}
-	else if (topFrame === type)
-	{
-		x.style.display = "none";
-	}
-	else
-	{
-		window.scrollTo(0, 0);
-	}
-	topFrame = type;
+    var x = document.getElementById("topFrame");
+    if (type === '' || type === null) {
+        x.style.display = "none";
+        topFrame = null;
+        return;
+    }
+    if (x.style.display === "none")
+    {
+        x.style.display = "block";
+        window.scrollTo(0, 0);
+    }
+    else if (topFrame === type)
+    {
+        x.style.display = "none";
+    }
+    else
+    {
+        window.scrollTo(0, 0);
+    }
+    topFrame = type;
 }
