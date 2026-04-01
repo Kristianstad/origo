226cd26c006e0971975ae51437f94cd051a457b9
Henrik Uggla <henrik.uggla@kristianstad.se>
2026-04-01 15:02:18 +0200

    Update ttl-parameter comments in iconTtl.php

--- finalfs/www/adm/constants/iconTtl.php
+++ finalfs/www/adm/constants/iconTtl.php
@@ -1,7 +1,5 @@
 <?php
 
-	// The size of the ttl-parameter added to the urls of layertree icons. Set to '0' if no ttl-parameter should be added.
-	// The ttl-parameter is never added to icon-urls of restricted layers (GROUP-layers are never considered restricted here).
+	// The size of the ttl-parameter added to the urls of layertree icons. Set to '-1' if no ttl-parameter should be added.
+	// Unless the ttl-parameter is set to '-1', the ttl-parameter is always set to '0' for icon-urls of restricted layers (GROUP-layers are never considered restricted here).
 	$iconTtl = '36000';
-
-?>
