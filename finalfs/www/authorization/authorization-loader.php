b3f164395a48ea6e0f9bff82bdbc23e560d9f4bc
Henrik Uggla <henrik.uggla@kristianstad.se>
2026-04-30 12:11:34 +0200

    Refactor authorization-loader.php to remove HTML
    Removed HTML structure and added PHP comments for clarity.

--- finalfs/www/authorization/authorization-loader.php
+++ finalfs/www/authorization/authorization-loader.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html style="width:100%;height:100%">
-	<head>
-		<base href='/php/adm/'>
-	</head>
-	<body>
-		<?php
-			chdir('../adm/');
-			require ('authorization.php');
-		?>
-	</body>
-</html>
+<?php
+	/**
+	 * REN loader för authorization (inloggning + displayLogin)
+	 * Används av Traefik ForwardAuth redirect + formulär-POST
+	 * Ingen HTML-output före PHP-logiken!
+	 */
+	chdir('../adm/');
+	require ('authorization.php');
