2094a7ee1fdbdeb5ac878ed15141fabd3d1abe01
Henrik Uggla <henrik.uggla@kristianstad.se>
2026-04-30 12:09:01 +0200

    Add displayHtmlHeader function for HTML header

---
+++ finalfs/www/adm/functions/authorization/displayHtmlHeader.php
@@ -0,0 +1,14 @@
+<?php
+
+function displayHtmlHeader()
+{
+	echo '<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<style>';
+	require("./styles/authorization.css");
+	echo '</style>
+</head>
+<body>';
+}
