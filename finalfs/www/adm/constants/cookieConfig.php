05a5f06f0df1f6bcd66c55c29443d8ebae486cdc
Henrik Uggla <henrik.uggla@kristianstad.se>
2026-05-26 13:06:54 +0200

    Update and rename cookieKey.php to cookieConfig.php

---
+++ finalfs/www/adm/constants/cookieConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+$cookieConfig = [
+	'cookieName'       => 'origo_user_id',
+	'cookieKey'        => 'Key8ffHEouFk3dsffFas42y6', // Used for encryption
+	'cookieLifetime'   => 60*60*24*30,
+    'cookieDomain'     => '.kristianstad.se',
+    'cookiePath'       => '/',
+    'cookieSecure'     => true,
+    'cookieHttpOnly'   => true,
+    'cookieSameSite'   => 'Lax',
+];
--- finalfs/www/adm/constants/cookieKey.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	// A cookie key (string) used for cookie encryption.
-	$cookieKey = 'vsaM85PstM&EfX6Hv5%7yFqQ';
-
-?>
