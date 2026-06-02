bb4668000b0589d21ae3742276fceed64434e87f
Henrik Uggla <henrik.uggla@kristianstad.se>
2026-06-02 12:27:05 +0200

    Update session configuration for longer durations

--- finalfs/www/adm/constants/forwardauthSessionConfig.php
+++ finalfs/www/adm/constants/forwardauthSessionConfig.php
@@ -7,11 +7,11 @@
 
 $forwardauthSessionConfig = [
     // Grundlivstid vid inloggning
-    'baseLifetime'     => 60 * 60 * 10,      // 10 timmar
+    'baseLifetime'     => 60 * 60 * 24 * 10,      // 10 dagar
 
     // Sliding expiration – hur mycket sessionen förlängs vid aktivitet
-    'slideExtension'   => 60 * 60 * 1,       // +1 timme per giltigt anrop
+    'slideExtension'   => 60 * 60 * 24 * 5,       // +5 dagar per giltigt anrop
 
     // Absolut max-tid en session får leva (säkerhetsgräns)
-    'absoluteMax'      => 60 * 60 * 24 * 10, // 10 dagar
+    'absoluteMax'      => 60 * 60 * 24 * 30,      // 30 dagar
 ];
