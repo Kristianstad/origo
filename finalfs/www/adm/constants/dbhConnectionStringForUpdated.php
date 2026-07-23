d3f3a2518a0459c7e1a2afeabdd55abfa460ea76
Henrik Uggla <henrik.uggla@kristianstad.se>
2026-07-23 10:22:40 +0200

    Add sslmode and connect_timeout to connection string

--- finalfs/www/adm/constants/dbhConnectionStringForUpdated.php
+++ finalfs/www/adm/constants/dbhConnectionStringForUpdated.php
@@ -1,6 +1,4 @@
 <?php
 
 	// The connection string used by updated.php (read only)
-	$dbhConnectionStringForUpdated = 'host=localhost port=5432 dbname=origo user=postgres password=postgres' ;
-
-?>
+	$dbhConnectionStringForUpdated = 'host=localhost port=5432 dbname=origo user=postgres password=postgres sslmode=prefer connect_timeout=5';
