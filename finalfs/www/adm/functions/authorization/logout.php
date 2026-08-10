4bb0593abc05f934c5f7d21d3f5fbf12f9b222a4
Henrik Uggla <henrik.uggla@kristianstad.se>
2026-08-10 10:36:57 +0200

    Refactor logout function with cookie settings
    Refactor logout function to use cookie configuration from constants and improve session handling.

--- finalfs/www/adm/functions/authorization/logout.php
+++ finalfs/www/adm/functions/authorization/logout.php
@@ -2,8 +2,31 @@
 
 	function logout()
 	{
-		setcookie('origo_user_id', '', time()-3600, '/', '', true, true);
-		session_start();
+		require('./constants/cookieConfig.php');
+		$cookieName = $cookieConfig['cookieName'];
+		$cookiestr = '';
+		$setcookieOptions =
+		[
+			'expires'  => time()-3600,
+			'path'     => $cookieConfig['cookiePath'],
+			'domain'   => $cookieConfig['cookieDomain'],
+			'secure'   => $cookieConfig['cookieSecure'],
+			'httponly' => $cookieConfig['cookieHttpOnly'],
+			'samesite' => $cookieConfig['cookieSameSite']
+		];
+		setcookie(
+			$cookieName, 
+			$cookiestr, 
+			$setcookieOptions
+		);
+		session_start([
+			'read_and_close'  => false,
+			'cookie_domain'   => $cookieConfig['cookieDomain'],
+			'cookie_path'     => $cookieConfig['cookiePath'],
+			'cookie_secure'   => $cookieConfig['cookieSecure'],
+			'cookie_httponly' => $cookieConfig['cookieHttpOnly'],
+			'cookie_samesite' => $cookieConfig['cookieSameSite']
+		]);
 		$_SESSION['user'] = false;
 		session_write_close();
 		echo '<b>Du är nu utloggad!</b><br>';
