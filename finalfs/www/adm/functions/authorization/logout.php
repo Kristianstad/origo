000339024681496dfe73f8330c0d4d7b4f814696
Henrik Uggla <henrik.uggla@kristianstad.se>
2026-08-10 11:00:58 +0200

    Refactor logout function for clarity and structure

--- finalfs/www/adm/functions/authorization/logout.php
+++ finalfs/www/adm/functions/authorization/logout.php
@@ -5,7 +5,7 @@ function logout()
 
     $cookieName = $cookieConfig['cookieName'];
 
-    // 1. Förstör sessionen ordentligt
+    // === 1. Förstör sessionen ordentligt ===
     if (session_status() === PHP_SESSION_NONE) {
         session_start([
             'cookie_domain'   => $cookieConfig['cookieDomain'],
@@ -16,49 +16,32 @@ function logout()
         ]);
     }
 
-    // Töm session-data
     $_SESSION = [];
 
-    // Ta bort session-cookien
+    // Ta bort PHP-session-cookien
     if (ini_get('session.use_cookies')) {
         $params = session_get_cookie_params();
-        setcookie(
-            session_name(),
-            '',
-            [
-                'expires'  => time() - 42000,
-                'path'     => $params['path'],
-                'domain'   => $params['domain'],
-                'secure'   => $params['secure'],
-                'httponly' => $params['httponly'],
-                'samesite' => $params['samesite'] ?? 'Lax',
-            ]
-        );
+        setcookie(session_name(), '', [
+            'expires'  => time() - 42000,
+            'path'     => $params['path'],
+            'domain'   => $params['domain'],
+            'secure'   => $params['secure'],
+            'httponly' => $params['httponly'],
+            'samesite' => $params['samesite'] ?? 'Lax',
+        ]);
     }
 
     session_destroy();
 
-    // 2. Ta bort din egen autentiseringscookie
-    setcookie(
-        $cookieName,
-        '',
-        [
-            'expires'  => time() - 3600,
-            'path'     => $cookieConfig['cookiePath'],
-            'domain'   => $cookieConfig['cookieDomain'],
-            'secure'   => $cookieConfig['cookieSecure'],
-            'httponly' => $cookieConfig['cookieHttpOnly'],
-            'samesite' => $cookieConfig['cookieSameSite'],
-        ]
-    );
+    // === 2. Ta bort autentiseringscookien + refresh-cookien ===
+    $options = getCookieOptions(time() - 3600);
 
-    // 3. Visa meddelande
+    setcookie($cookieName, '', $options);
+    setcookie($cookieName . '_last_refresh', '', $options);
+
+    // === 3. Visa meddelande ===
     echo '<b>Du är nu utloggad!</b><br>';
     displayLogin();
-
-    if (function_exists('fastcgi_finish_request')) {
-        fastcgi_finish_request();
-    }
 
     exit;
 }
