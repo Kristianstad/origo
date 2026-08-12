cb222b3c8e7b31188726ff704e13d716f83ede02
Henrik Uggla <henrik.uggla@kristianstad.se>
2026-08-12 12:28:44 +0200

    Refactor news.php for improved structure and clarity

--- finalfs/www/adm/news.php
+++ finalfs/www/adm/news.php
@@ -1,88 +1,92 @@
+<?php
+header("Cache-Control: must-revalidate, max-age=0, s-maxage=0, no-cache, no-store");
+
+require_once("./functions/includeDirectory.php");
+includeDirectory("./functions/common");
+includeDirectory("./functions/news");
+
+require './constants/authMethod.php';
+
+readAndCloseSession();
+$dbh = null;
+
+if ($authMethod === 'ldap') {
+    $dbh = dbh();
+    initUserLdap($dbh);
+}
+
+$action = $_GET['action'] ?? '';
+$newId  = $_GET['newId']  ?? '';
+$return = isset($_GET['return']) ? explode(',', $_GET['return']) : [];
+
+$isLoggedIn = isset($_SESSION['user']) && $_SESSION['user'] !== false;
+
+// === Början av sidan ===
+echo <<<HTML
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<style>
-		<?php require("./styles/news.css"); ?>
-	</style>
+<meta charset="utf-8">
+<style>
+HTML;
+
+require "./styles/news.css";
+
+echo <<<HTML
+</style>
 </head>
 <body>
-<?php
-	// Tell browsers to not cache response
-	header("Cache-Control: must-revalidate, max-age=0, s-maxage=0, no-cache, no-store");
-	
-	// Expose specific functions
-	require_once("./functions/includeDirectory.php");
-	
-	// Expose all functions in given folders
-	includeDirectory("./functions/common");
-	includeDirectory("./functions/news");
-	
-	require './constants/cookieConfig.php';
-	require './constants/authMethod.php';
+HTML;
 
-	session_start([
-		'read_and_close'  => true,
-		'cookie_domain'   => $cookieConfig['cookieDomain'],
-		'cookie_path'     => $cookieConfig['cookiePath'],
-		'cookie_secure'   => $cookieConfig['cookieSecure'],
-		'cookie_httponly' => $cookieConfig['cookieHttpOnly'],
-		'cookie_samesite' => $cookieConfig['cookieSameSite']
-	]);
+// === Innehåll ===
+if ($isLoggedIn) {
+    ignore_user_abort(true);
 
-	$dbh = null;
-	if ($authMethod === 'ldap')
-	{
-		$dbh=dbh();
-		initUserLdap($dbh);
-	}
-	if (isset($_SESSION['user']) && $_SESSION['user'] !== false)
-	{
-		ignore_user_abort(true); 		
-		$username=$_SESSION['user']['id'];
-		if ($authMethod !== 'ldap')
-		{
-			$dbh=dbh();
-		}
-		$pgNewsArray=pgNewsArray($dbh);
-		$userNews=userNews($username, $pgNewsArray);
-		if (!empty($_GET['newId']))
-		{
-			$newId=$_GET['newId'];
-			$selectedNew= selectNew($userNews, $newId);
-		}
-		$action=$_GET['action'];
-		if ($action === 'list')
-		{
-			printNewsList($userNews);
-		}
-		elseif ($action === 'load' && !empty($selectedNew))
-		{
-			$return=explode(',', $_GET['return']);
-			printNews($username, $selectedNew, $return);
-		}
-		elseif (($action === 'delete' || $action === 'read') && !empty($selectedNew) && !in_array($username, $selectedNew[$action.'s']))
-		{
-			readDelete($username, $selectedNew, $action);
-		}
-		elseif ($action === 'subjects')
-		{
-			printNewsSubjects($username, $userNews);
-		}
-		elseif ($action === 'unread')
-		{
-			testUnread($username, $userNews);
-		}
-		ignore_user_abort(false); 
-	}
-	else
-	{
-		echo '<b style="color:#000000">Ej inloggad!</b>';
-	}
-	if (isset($dbh) && $dbh)
-	{
-		pg_close($dbh);
-	}
-?>
+    $username = $_SESSION['user']['id'];
+
+    if ($authMethod !== 'ldap') {
+        $dbh = dbh();
+    }
+
+    $pgNewsArray = pgNewsArray($dbh);
+    $userNews    = userNews($username, $pgNewsArray);
+
+    $selectedNew = null;
+    if ($newId !== '') {
+        $selectedNew = selectNew($userNews, $newId);
+    }
+
+    if ($action === 'list') {
+        printNewsList($userNews);
+    }
+    elseif ($action === 'load' && !empty($selectedNew)) {
+        printNews($username, $selectedNew, $return);
+    }
+    elseif (
+        ($action === 'delete' || $action === 'read') &&
+        !empty($selectedNew) &&
+        !in_array($username, $selectedNew[$action . 's'] ?? [], true)
+    ) {
+        readDelete($username, $selectedNew, $action);
+    }
+    elseif ($action === 'subjects') {
+        printNewsSubjects($username, $userNews);
+    }
+    elseif ($action === 'unread') {
+        testUnread($username, $userNews);
+    }
+
+    ignore_user_abort(false);
+} else {
+    echo '<b style="color:#000000">Ej inloggad!</b>';
+}
+
+if (isset($dbh) && $dbh) {
+    pg_close($dbh);
+}
+
+// === Slut på sidan ===
+echo <<<HTML
 </body>
 </html>
+HTML;
