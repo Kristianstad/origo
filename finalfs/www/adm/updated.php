00ce3281d3dd36d8425d3028a7c08fdf3e515b74
Henrik Uggla <henrik.uggla@kristianstad.se>
2026-08-13 11:08:59 +0200

    Refactor updated.php for improved structure
    Refactored updated.php to improve structure and readability by removing redundant code and echoing the updated value directly.

--- finalfs/www/adm/updated.php
+++ finalfs/www/adm/updated.php
@@ -1,38 +1,49 @@
+<?php
+// Tell browsers to not cache response
+header("Cache-Control: must-revalidate, max-age=0, s-maxage=0, no-cache, no-store");
+
+// Expose specific functions
+require_once("./functions/includeDirectory.php");
+
+// Expose all functions in given folders
+includeDirectory("./functions/common");
+includeDirectory("./functions/updated");
+
+require("./constants/dbhConnectionStringForUpdated.php");
+$dbh = dbh($dbhConnectionStringForUpdated);
+
+$tablesWithSchema = $_GET['table'] ?? '';
+$tablesWithSchema = explode(',', $tablesWithSchema);
+
+$updates = array();
+foreach ($tablesWithSchema as $tableWithSchema) {
+    $updated = updated_from_table2($dbh, $tableWithSchema);
+    if (isset($updated[1])) {
+        $updates[$updated[0]] = $updated[1];
+    }
+}
+
+pg_close($dbh);
+
+arsort($updates);
+$updated = key($updates);
+$updated = substr($updated, 0, 10);
+
+// === Början av sidan ===
+echo <<<HTML
 <!DOCTYPE html>
 <html>
 <head>
 	<style>
-		<?php require("./styles/updated.css"); ?>
+HTML;
+
+require("./styles/updated.css");
+
+echo <<<HTML
 	</style>
 </head>
 <body>
-<?php
-	// Tell browsers to not cache response
-	header("Cache-Control: must-revalidate, max-age=0, s-maxage=0, no-cache, no-store");
-
-	// Expose specific functions
-	require_once("./functions/includeDirectory.php");
-
-	// Expose all functions in given folders
-	includeDirectory("./functions/common");
-	includeDirectory("./functions/updated");
-
-	require("./constants/dbhConnectionStringForUpdated.php");
-	$dbh=dbh($dbhConnectionStringForUpdated);
-	$tablesWithSchema=$_GET['table'];
-	$tablesWithSchema=explode(',', $tablesWithSchema);
-	$updates=array();
-	foreach ($tablesWithSchema as $tableWithSchema)
-	{
-		$updated=updated_from_table2($dbh, $tableWithSchema);
-		if (isset($updated[1]))
-		{
-			$updates[$updated[0]]=$updated[1];
-		}
-	}
-	pg_close($dbh);
-	arsort($updates);
-	$updated=key($updates);
-	$updated=substr($updated, 0, 10);
-	echo $updated;
-?>
+{$updated}
+</body>
+</html>
+HTML;
