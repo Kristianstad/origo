416b31a9d4d061f16dae9d13b69eaab980fff81e
Henrik Uggla <henrik.uggla@kristianstad.se>
2026-08-13 10:36:33 +0200

    Refactor help.php to streamline content generation

--- finalfs/www/adm/help.php
+++ finalfs/www/adm/help.php
@@ -1,8 +1,46 @@
+<?php
+// Tell browsers to not cache response
+header("Cache-Control: must-revalidate, max-age=0, s-maxage=0, no-cache, no-store");
+
+$content = '';
+
+if (isset($_GET['id'])) {
+    // Expose specific functions
+    require_once("./functions/includeDirectory.php");
+
+    // Expose all functions in given folders
+    includeDirectory("./functions/common");
+
+    require("./constants/configSchema.php");
+    $dbh = dbh();
+    $helps = all_from_table($dbh, $configSchema, 'helps');
+    pg_close($dbh);
+
+    $help = array_column_search($_GET['id'], 'help_id', $helps);
+    if (isset($help['abstract'])) {
+        $content = $help['abstract'];
+    }
+} else {
+    $content = <<<HERE
+				<a href="../Origo_admin_tutorial_swedish.pdf" target="_blank">Origo admin tutorial</a><br>
+				<a href="https://origo-map.github.io/origo-documentation/latest/#origo-map" target="_blank">Origo-dokumentation</a><br>
+				<a href="https://jsonchecker.com/" target="_blank">JSON Checker</a>
+HERE;
+}
+
+$content .= "<br style=\"clear:both\"><button type=\"button\" onclick=\"window.parent.postMessage({ action: 'close' }, window.location.origin);\">Stäng</button>";
+
+// === Början av sidan ===
+echo <<<HTML
 <!DOCTYPE html>
 <html>
 <head>
 	<style>
-		<?php require("./styles/help.css"); ?>
+HTML;
+
+require("./styles/help.css");
+
+echo <<<HTML
 	</style>
 	<script>
 		window.onload = function() {
@@ -13,37 +51,7 @@
 	</script>
 </head>
 <body>
-	<?php
-		// Tell browsers to not cache response
-		header("Cache-Control: must-revalidate, max-age=0, s-maxage=0, no-cache, no-store");
-		
-		if (isset($_GET['id']))
-		{
-			// Expose specific functions
-			require_once("./functions/includeDirectory.php");
-			
-			// Expose all functions in given folders
-			includeDirectory("./functions/common");
-			
-			require("./constants/configSchema.php");
-			$dbh=dbh();
-			$helps=all_from_table($dbh, $configSchema, 'helps');
-			pg_close($dbh);
-			$help=array_column_search($_GET['id'], 'help_id', $helps);
-			if (isset($help['abstract']))
-			{
-				echo $help['abstract'];
-			}
-		}
-		else
-		{
-			echo <<<HERE
-				<a href="../Origo_admin_tutorial_swedish.pdf" target="_blank">Origo admin tutorial</a><br>
-				<a href="https://origo-map.github.io/origo-documentation/latest/#origo-map" target="_blank">Origo-dokumentation</a><br>
-				<a href="https://jsonchecker.com/" target="_blank">JSON Checker</a>
-			HERE;
-		}
-		echo "<br style=\"clear:both\"><button type=\"button\" onclick=\"window.parent.postMessage({ action: 'close' }, window.location.origin);\">Stäng</button>";
-	?>
+{$content}
 </body>
 </html>
+HTML;
