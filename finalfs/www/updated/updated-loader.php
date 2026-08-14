ba65026e1528ad06e987f2ecbcd6b51d5bd51f64
Henrik Uggla <henrik.uggla@kristianstad.se>
2026-08-14 14:54:54 +0200

    Refactor updated-loader.php to remove HTML
    Removed HTML structure and retained only PHP code for loading.

--- finalfs/www/updated/updated-loader.php
+++ finalfs/www/updated/updated-loader.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html style="width:100%;height:100%">
-	<head>
-		<base href='/php/adm/'>
-	</head>
-	<body>
-		<?php
-			chdir('../adm/');
-			require ('updated.php');
-		?>
-	</body>
-</html>
+<?php
+	chdir('../adm/');
+	require ('updated.php');
