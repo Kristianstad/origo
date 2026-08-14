cc90129ad5eacf64f0c88eb60a523b1b50db9656
Henrik Uggla <henrik.uggla@kristianstad.se>
2026-08-14 14:40:39 +0200

    Refactor news-loader.php to PHP only
    Removed HTML structure and retained PHP code for loading news.

--- finalfs/www/authorization/news-loader.php
+++ finalfs/www/authorization/news-loader.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html style="width:100%;height:100%">
-	<head>
-		<base href='/php/adm/'>
-	</head>
-	<body>
-		<?php
-			chdir('../adm/');
-			require ('news.php');
-		?>
-	</body>
-</html>
+<?php
+	chdir('../adm/');
+	require ('news.php');
