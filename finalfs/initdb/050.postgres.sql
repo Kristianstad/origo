70614d2f9a205370257a80620d5132799b0ce0d8
Henrik Uggla <henrik.uggla@kristianstad.se>
2026-09-16 11:52:49 +0200

    Make database creation idempotent

--- finalfs/initdb/050.postgres.sql
+++ finalfs/initdb/050.postgres.sql
@@ -1,1 +1,3 @@
-CREATE DATABASE origo;
+-- Create the application database only when it does not already exist.
+SELECT 'CREATE DATABASE origo'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'origo')\gexec
